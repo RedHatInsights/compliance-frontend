be099dfb31c47c3e9166acfb26c45d136e2a9991
Daniel Lobato García <me@daniellobato.me>
2018-11-08 18:11:41 +0100

    Wait for getUser before making any API calls (#4)
    We need to ensure the calls go authenticated

--- src/Utilities/ComplianceBackend/ComplianceBackendAPI.js
+++ src/Utilities/ComplianceBackend/ComplianceBackendAPI.js
@@ -1,15 +1,18 @@
 import { API_HEADERS, COMPLIANCE_API_ROOT } from '../../constants';
 
-export function getProfiles(apiProps) {
-    return fetch(COMPLIANCE_API_ROOT.concat('/profiles'), {
-        method: 'get',
-        headers: API_HEADERS,
-        body: JSON.stringify(apiProps)
-    }).then(function(response) {
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
+export const getProfiles = apiProps => {
+    return window.insights.chrome.auth.getUser()
+    .then(() => {
+        return fetch(COMPLIANCE_API_ROOT.concat('/profiles'), {
+            method: 'get',
+            headers: API_HEADERS,
+            body: JSON.stringify(apiProps)
+        }).then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
 
-        return response.json();
+            return response.json();
+        });
     });
-}
+};
