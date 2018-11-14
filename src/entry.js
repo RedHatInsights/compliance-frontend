fd7cee3edf5e68708d70ddc3a7008cefe7725bba
Daniel Lobato Garcia <me@daniellobato.me>
2018-11-14 23:13:28 +0100

    Remove unused constant

--- src/entry.js
+++ src/entry.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { init } from './store';
 import App from './App';
-import { API_WS_ROOT } from './constants';
 
 ReactDOM.render(
     <Provider store={ init().getStore() }>
