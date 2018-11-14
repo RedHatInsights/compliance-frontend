a49910608758bf688a6266ace02a0b253d21ffa5
Daniel Lobato García <me@daniellobato.me>
2018-11-14 15:53:30 +0100

    Change URL for API calls (#5)

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import { version } from './../package.json';
-export const COMPLIANCE_API_ROOT = 'http://localhost:3000';
+export const COMPLIANCE_API_ROOT = 'https://prod.foo.redhat.com:1337/r/insights/platform/compliance';
 export const COMPLIANCE_WS_ROOT = process.env.NODE_ENV === 'production'
     ? 'wss://localhost:3000/cable'
     : 'ws://localhost:3000/cable';
