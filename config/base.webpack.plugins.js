c969256f6156d2f8a6d12ee216232948976d4e26
Daniel Lobato Garcia <me@daniellobato.me>
2018-11-23 14:28:01 +0100

    Fix linting on config file for lodash

--- config/base.webpack.plugins.js
+++ config/base.webpack.plugins.js
@@ -51,7 +51,12 @@ plugins.push(CleanWebpackPlugin);
  *
  * @type {var}
  */
-const LodashWebpackPlugin = new (require('lodash-webpack-plugin'))({ shorthands: true, currying: true, flattening: true, placeholders: true });
+const LodashWebpackPlugin = new (require('lodash-webpack-plugin'))({
+    shorthands: true,
+    currying: true,
+    flattening: true,
+    placeholders: true
+});
 plugins.push(LodashWebpackPlugin);
 
 /**
