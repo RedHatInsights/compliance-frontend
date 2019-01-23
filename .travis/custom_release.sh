5271d7baaa387a6d07a6ed0f3bfcf98469fcf8fd
Daniel Lobato Garcia <me@daniellobato.me>
2019-01-23 10:48:47 +0100

    Support stable branch

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,17 +2,23 @@
 set -e
 set -x
 
-# for now in chrome... push everywhere when master updates
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     for env in ci qa prod
     do
-        for release in stable beta
-        do
-            echo
-            echo
-            echo "PUSHING ${env}-${release}"
-	    rm -rf dist/.git
-            .travis/release.sh "${env}-${release}"
-        done
+       echo
+       echo
+       echo "PUSHING ${env}-beta"
+       rm -rf dist/.git
+       .travis/release.sh "${env}-beta"
+    done
+fi
+if [ "${TRAVIS_BRANCH}" = "stable" ]; then
+    for env in ci qa prod
+    do
+	echo
+	echo
+	echo "PUSHING ${env}-stable"
+	rm -rf dist/.git
+	.travis/release.sh "${env}-stable"
     done
 fi
