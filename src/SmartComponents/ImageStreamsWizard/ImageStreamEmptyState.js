cfda090a6e2b1902b3ba8c0ab007da60d6b67f42
Andrew Kofink <akofink@redhat.com>
2019-04-04 15:34:17 -0400

    Update imagestreams empty state with valid DOM

--- src/SmartComponents/ImageStreamsWizard/ImageStreamEmptyState.js
+++ src/SmartComponents/ImageStreamsWizard/ImageStreamEmptyState.js
@@ -5,11 +5,11 @@ import {
     Bullseye,
     EmptyState,
     EmptyStateIcon,
-    EmptyStateBody,
     Button
 } from '@patternfly/react-core';
 import { ClipboardCheckIcon } from '@patternfly/react-icons';
 import PropTypes from 'prop-types';
+import emptyStateStyles from '@patternfly/patternfly/components/EmptyState/empty-state.css';
 
 const ImageStreamEmptyState = ({ openWizard }) => (
     <Bullseye>
@@ -17,13 +17,13 @@ const ImageStreamEmptyState = ({ openWizard }) => (
             <EmptyStateIcon size="xl" title="Compliance" icon={ClipboardCheckIcon} />
             <br/>
             <Title size="lg">No Image Streams</Title>
-            <EmptyStateBody>
+            <span className={emptyStateStyles.emptyStateBody}>
                 <TextContent>
                     You have not added any Image Streams to scan yet.<br/>
 
                     Add an Image Stream to scan and view compliance reports.
                 </TextContent>
-            </EmptyStateBody>
+            </span>
             <Button variant='primary' onClick={openWizard}> Scan an Image Stream </Button>
         </EmptyState>
     </Bullseye>
