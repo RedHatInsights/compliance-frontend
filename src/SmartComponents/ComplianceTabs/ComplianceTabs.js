a10e2f2e4ea3b0e198acc71fa244664b4326b972
Daniel Lobato García <me@daniellobato.me>
2019-05-02 09:04:23 -0400

    Revert showing conditionally imagestream tab (#129)

--- src/SmartComponents/ComplianceTabs/ComplianceTabs.js
+++ src/SmartComponents/ComplianceTabs/ComplianceTabs.js
@@ -6,10 +6,7 @@ import { Tabs, Tab } from '@patternfly/react-core';
 import invert from 'lodash/invert';
 
 class ComplianceTabs extends Component {
-    _isMounted = false;
-
     state = {
-        internalUser: false,
         tabPaths: {
             0: paths.compliancePolicies,
             1: paths.complianceSystems
@@ -21,46 +18,15 @@ class ComplianceTabs extends Component {
         this.props.history.push(tabPaths[tabIndex]);
     }
 
-    componentDidMount() {
-        this._isMounted = true;
-        window.insights.chrome.auth.getUser().then((data) => {
-            if (this._isMounted) {
-                this.setState({
-                    internalUser: data.identity.user.is_internal,
-                    tabPaths: {
-                        0: paths.compliancePolicies,
-                        1: paths.complianceSystems,
-                        2: paths.complianceImageStreams
-                    }
-                });
-            }
-        });
-    }
-
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
-
     render() {
         const { match: { path } } = this.props;
-        const { internalUser, tabPaths } = this.state;
+        const { tabPaths } = this.state;
         let currentKey = Number(invert(tabPaths)[path]);
 
         const tabs = [
             <Tab title={'Policies'} key={0} eventKey={0}></Tab>,
             <Tab title={'Systems'} key={1} eventKey={1}></Tab>
         ];
-
-        if (internalUser) {
-            tabs.push(<Tab title={'Image Streams'} key={2} eventKey={2}></Tab>);
-        } else if (path === '/imagestreams') {
-            // We cannot pass '2' to activeKey while the page is loading
-            // on the imagestreams page. This breaks the component.
-            // While the page is loading, we don't know yet if this
-            // an internal user or not. Therefore we just mark '1' as the
-            // activeKey until we know if it's an internalUser in /imagestreams
-            currentKey = 1;
-        }
 
         return (
             <Tabs activeKey={currentKey} onSelect={this.redirect} aria-label="Compliance Tabs">
