1619c6c0059e7432f941ea42d00a7706dbe9102e
Andrew Kofink <akofink@redhat.com>
2019-05-17 10:55:55 -0400

    Update spandx to match the sample app example (#145)

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,17 +1,16 @@
-/*global module, process*/
+/*global module*/
 
-// Hack so that Mac OSX docker can sub in host.docker.internal instead of localhost
-// see https://docs.docker.com/docker-for-mac/networking/#i-want-to-connect-from-a-container-to-a-service-on-the-host
-const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
-const host = 'https://insights-inventory-platform-ci.5a9f.insights-dev.openshiftapps.com';
+const SECTION = 'rhel';
+const APP_ID = 'compliance';
+const FRONTEND_PORT = 8002;
+const API_PORT = 3000;
+const routes = {};
 
-module.exports = {
-    routes: {
-        '/apps/compliance': { host: `http://${localhost}:8002` },
-        '/rhcs/compliance': { host: `http://${localhost}:8002` },
-        '/r/insights/platform': { host: 'http://access.ci.cloud.paas.upshift.redhat.com' },
-        '/apps/chrome': { host: 'https://ci.cloud.paas.upshift.redhat.com' },
-        '/api/compliance': { host: `http://${localhost}:3000` },
-        '/r/insights/platform/inventory/': { host } // for CI env
-    }
-};
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
+
+routes[`/api/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
+
+module.exports = { routes };
