d0834fcf7ed415f9e1fdfaeb777af1f6198dca67
Sebastian Gräßl <bastilian@users.noreply.github.com>
2019-10-24 20:37:39 +0200

    RHICOMPL-393 - Set placeholder according to mockup (#204)

--- src/SmartComponents/EditPolicy/BusinessObjectiveField.js
+++ src/SmartComponents/EditPolicy/BusinessObjectiveField.js
@@ -24,7 +24,7 @@ class BusinessObjectiveField extends React.Component {
         this.state = {
             policyId: props.policyId,
             isExpanded: false,
-            originalValue: props.businessObjective ? props.businessObjective.title : '',
+            originalValue: props.businessObjective ? props.businessObjective.title : 'Type to select and create',
             selected: props.businessObjective ? props.businessObjective.id : '',
             options: [],
             originalOptions: [],
