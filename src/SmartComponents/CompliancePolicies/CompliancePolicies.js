0ac16dd3b1f62b439648b6c77aad2ce7f625f19a
Daniel Lobato García <me@daniellobato.me>
2019-11-04 09:28:46 +0100

    Display only profiles with at least 1 host (#208)

--- src/SmartComponents/CompliancePolicies/CompliancePolicies.js
+++ src/SmartComponents/CompliancePolicies/CompliancePolicies.js
@@ -96,7 +96,8 @@ const CompliancePolicies = () => {
         );
     }
 
-    const policies = data.allProfiles;
+    const policies = data.allProfiles.filter((profile) => profile.totalHostCount > 0);
+
     let policyCards = [];
     let pageHeader;
     if (policies.length) {
