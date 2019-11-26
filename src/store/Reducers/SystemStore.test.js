58d45340b6bec421f4f01d8c75f197928f91cb41
Daniel Lobato Garcia <me@daniellobato.me>
2019-11-26 11:42:00 +0100

    Tests for systemsToInventoryEntities

--- src/store/Reducers/SystemStore.test.js
+++ src/store/Reducers/SystemStore.test.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { QuestionCircleIcon, CheckCircleIcon, ExclamationCircleIcon } from '@patternfly/react-icons';
-import { compliantIcon, rulesCount, lastScanned } from './SystemStore';
+import {
+    compliantIcon,
+    rulesCount,
+    lastScanned,
+    systemsToInventoryEntities
+} from './SystemStore';
+import { systems, entities } from './SystemStore.fixtures';
 
-describe('SystemsStore:', () => {
+describe('mapping systems to inventory entities', () => {
+    it('should return an empty set if there are no systems', () => {
+        expect(systemsToInventoryEntities([], entities)).toEqual([]);
+    });
+
+    it('should only return systems with a matching inventory entity', () => {
+        const oneMatchingEntityInventory = [{ id: 'd5bc2459-21ce-4d11-bc0b-03ea7513dfa6', facts: [] }];
+        const inventoryEntities = systemsToInventoryEntities(systems, oneMatchingEntityInventory);
+        expect(inventoryEntities.length).toBe(1);
+    });
+
+    it('should only return all systems if all have a matching in the inventory', () => {
+        const inventoryEntities = systemsToInventoryEntities(systems, entities);
+        const systemIds = systems.map(system => system.node.id);
+        const systemNames = systems.map(system => system.node.name);
+        const systemComplianceScores = [' 40%', ' N/A'];
+        expect(inventoryEntities.length).toBe(2);
+        expect(inventoryEntities.map(entity => entity.id).sort()).toEqual(systemIds.sort());
+        expect(inventoryEntities.map(entity => entity.display_name).sort()).toEqual(systemNames.sort());
+        expect(inventoryEntities.map(entity => entity.facts.compliance.compliance_score_text).sort()).
+        toEqual(systemComplianceScores.sort());
+    });
+});
+
+describe('auxiliary functions to reducer', () => {
     it('should set rules count as the sum over all profiles', () => {
         const system = {
             profiles: [
