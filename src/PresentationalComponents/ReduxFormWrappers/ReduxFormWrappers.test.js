13718a08536d8e13c1502c9377ea057f48d975f5
Sebastian Gräßl <mail@bastilian.me>
2019-11-27 13:52:12 +0100

    Move ReduxFormWrappers to Presentational Components

---
+++ src/PresentationalComponents/ReduxFormWrappers/ReduxFormWrappers.test.js
@@ -0,0 +1,73 @@
+import toJson from 'enzyme-to-json';
+
+import {
+  ReduxFormTextInput,
+  ReduxFormCheckboxInput,
+  ReduxFormCreatableSelectInput
+} from './ReduxFormWrappers';
+
+describe('ReduxFormTextInput', () => {
+    it('expect to render without error', () => {
+        const field = {
+          input: {
+            onChange: jest.fn(),
+            value: 'Value'
+          },
+          additional_prop: 'Prop1'
+        }
+        const wrapper = shallow(
+            <ReduxFormTextInput { ...field } />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('expect to render with defaultValue', () => {
+        const field = {
+          input: {
+            onChange: jest.fn()
+          },
+          defaultValue: 'Default Value',
+          additional_prop: 'Prop1'
+        }
+        const wrapper = shallow(
+            <ReduxFormTextInput { ...field } />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+describe('ReduxFormCheckboxInput', () => {
+    it('expect to render without error', () => {
+        const field = {
+          input: {
+            onChange: jest.fn(),
+            value: 'Value'
+          },
+          additional_prop: 'Prop1'
+        }
+        const wrapper = shallow(
+            <ReduxFormCheckboxInput { ...field } />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+describe('ReduxFormCreatableSelectInput', () => {
+    it('expect to render without error', () => {
+        const field = {
+          input: {
+            onChange: jest.fn()
+          },
+          selected: 'SELECTED_VALUE',
+          additional_prop: 'Prop1'
+        }
+        const wrapper = shallow(
+            <ReduxFormCreatableSelectInput { ...field } />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
