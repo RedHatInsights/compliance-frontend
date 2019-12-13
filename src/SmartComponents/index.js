1363e90ff2a320db45f81bc4f6bb6bd491dcb08e
Sebastian Gräßl <mail@bastilian.me>
2019-12-13 11:44:29 +0100

    Export missing components

--- src/SmartComponents/index.js
+++ src/SmartComponents/index.js
@@ -1,2 +1,4 @@
 export { default as ComplianceRemediationButton } from './ComplianceRemediationButton/ComplianceRemediationButton';
 export { default as DownloadTableButton } from './DownloadTableButton/DownloadTableButton';
+export { default as ComplianceSystems } from './ComplianceSystems/ComplianceSystems';
+export { default } from './InventoryDetails/InventoryDetails';
