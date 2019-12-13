47f44fefb60da686e957aac0b259d84d5a54dbe2
Sebastian Gräßl <mail@bastilian.me>
2019-12-13 11:37:47 +0100

    Add tests for ComplianceSystems component

---
+++ src/SmartComponents/ComplianceSystems/ComplianceSystems.test.js
@@ -0,0 +1,13 @@
+import toJson from 'enzyme-to-json';
+
+import ComplianceSystems from './ComplianceSystems.js';
+
+describe('ComplianceSystems', () => {
+    it('expect to render without error', () => {
+        const wrapper = shallow(
+            <ComplianceSystems />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
