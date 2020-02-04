42696a4896f13fe471aa5e203710378c79efcb5f
Daniel Lobato Garcia <me@daniellobato.me>
2020-02-04 17:47:17 +0100

    Handle failures on mutation

--- src/SmartComponents/DeletePolicy/DeletePolicy.js
+++ src/SmartComponents/DeletePolicy/DeletePolicy.js
@@ -7,40 +7,53 @@ import propTypes from 'prop-types';
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { DELETE_PROFILE } from '../../Utilities/graphql/mutations';
+import { addNotification } from '@redhat-cloud-services/frontend-components-notifications';
+import { dispatchAction } from '../../Utilities/Dispatcher';
 
 const DeletePolicy = ({ isModalOpen, policy, toggle, onDelete }) => {
-    const [deletePolicy] = useMutation(DELETE_PROFILE);
+    const [deletePolicy] = useMutation(DELETE_PROFILE, {
+        onCompleted: () => {
+            dispatchAction(addNotification({
+                variant: 'success',
+                title: `Removed policy ${policy.name}`
+            }));
+            onDelete();
+            toggle();
+        },
+        onError: (error) => {
+            dispatchAction(addNotification({
+                variant: 'danger',
+                title: 'Error removing policy',
+                description: error.message
+            }));
+            onDelete();
+            toggle();
+        }
+    });
     const { name, id } = policy;
 
     return (
-        <React.Fragment>
-            <Modal
-                isSmall
-                title='Delete policy'
-                isOpen={isModalOpen}
-                onClose={toggle}
-                actions={[
-                    <Button key='destroy' variant='danger'
-                        onClick={() => {
-                            deletePolicy({ variables: { input: { id } } });
-                            onDelete();
-                            toggle();
-                        }} >
-                        Delete policy
-                    </Button>,
-                    <Button key='cancel' variant='secondary' onClick={toggle}>
-                        Cancel
-                    </Button>
-                ]}
-            >
-                <TextContent>
-                    Are you sure you want to delete <b>{ name }</b>?
-                </TextContent>
-                <TextContent>
-                    This cannot be undone.
-                </TextContent>
-            </Modal>
-        </React.Fragment>
+        <Modal
+            isSmall
+            title='Delete policy'
+            isOpen={isModalOpen}
+            onClose={toggle}
+            actions={[
+                <Button key='destroy' variant='danger' onClick={() => deletePolicy({ variables: { input: { id } } })}>
+                    Delete policy
+                </Button>,
+                <Button key='cancel' variant='secondary' onClick={toggle}>
+                    Cancel
+                </Button>
+            ]}
+        >
+            <TextContent>
+                Are you sure you want to delete <b>{ name }</b>?
+            </TextContent>
+            <TextContent>
+                This cannot be undone.
+            </TextContent>
+        </Modal>
     );
 };
 
