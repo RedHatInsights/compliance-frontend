1f0c189f889bf07e6ef642000e9158be4d4fb47b
Daniel Lobato Garcia <me@daniellobato.me>
2020-02-11 15:55:34 +0100

    Resolve link to policy results correctly if not in page 1

--- src/SmartComponents/PoliciesTable/PoliciesTable.test.js
+++ src/SmartComponents/PoliciesTable/PoliciesTable.test.js
@@ -1,6 +1,16 @@
 import toJson from 'enzyme-to-json';
 import { policies } from './fixtures.js';
 import debounce from 'lodash/debounce';
+import { history } from 'react-router-dom';
+
+jest.mock('react-router-dom', () => (
+    {
+        history: {
+            push: jest.fn()
+        },
+        withRouter: jest.fn()
+    }
+));
 
 jest.mock('lodash/debounce');
 jest.mock('@apollo/react-hooks');
@@ -12,8 +22,7 @@ jest.mock('@redhat-cloud-services/frontend-components-inventory-compliance', () 
     const ComplianceRemediationButton = () => <button>Remediations</button>;
     return ComplianceRemediationButton;
 });
-
-import { PoliciesTable } from './PoliciesTable.js';
+import { PoliciesTable, policiesToRows } from './PoliciesTable.js';
 
 describe('PoliciesTable', () => {
     it('expect to render without error', () => {
@@ -43,145 +52,49 @@ describe('PoliciesTable', () => {
         });
 
         it('should show only matching results after searching', async () => {
-            const searchResults = [
-                {
-                    cells: [
-                        'Red Hat Corporate Profile for Certified Cloud Providers (RH CCP)',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                }
-            ];
-
+            const filteredProfiles = policies.edges.filter(row => row.node.name.match(/CCP/)).map(profile => profile.node);
+            const searchResults = policiesToRows(filteredProfiles);
             const instance = wrapper.instance();
             await instance.handleSearch('CCP');
             expect(wrapper.state('currentRows')).toEqual(searchResults);
         });
 
         it('should be able to move to next and previous pages', async () => {
-            const firstPage = [
-                {
-                    cells: [
-                        'United States Government Configuration Baseline23',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                },
-                {
-                    cells: [
-                        'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 73',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '67%'
-                    ]
-                },
-                {
-                    cells: [
-                        'United States Government Configuration Baseline2',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                },
-                {
-                    cells: [
-                        'C2S for Red Hat Enterprise Linux 7',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '69.5%'
-                    ]
-                },
-                {
-                    cells: [
-                        'Criminal Justice Information Services (CJIS) Security Policy',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                },
-                {
-                    cells: [
-                        'Unclassified Information in Non-federal Information Systems and Organizations (NIST 800-171)',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                },
-                {
-                    cells: [
-                        'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 7',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                },
-                {
-                    cells: [
-                        'Red Hat Corporate Profile for Certified Cloud Providers (RH CCP)',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                },
-                {
-                    cells: [
-                        'Health Insurance Portability and Accountability Act (HIPAA)',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                },
-                {
-                    cells: [
-                        'United States Government Configuration Baseline',
-                        'RHEL 7',
-                        10,
-                        '--',
-                        '100%'
-                    ]
-                }
-            ];
-            const secondPage = [
-                { cells: [
-                    'Standard System Security Profile for Red Hat Enterprise Linux 7',
-                    'RHEL 7',
-                    10,
-                    '--',
-                    '100%'
-                ] },
-                { cells: [
-                    'DISA STIG for Red Hat Enterprise Linux 7',
-                    'RHEL 7',
-                    10,
-                    '--',
-                    '100%'
-                ] },
-                { cells: [
-                    'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 72',
-                    'RHEL 7',
-                    10,
-                    '--',
-                    '100%'
-                ] }
-            ];
-
+            const pageOnePolicies = policiesToRows(
+                policies.edges.slice(0, 10).map(profile => profile.node)
+            );
+            const pageTwoPolicies = policiesToRows(
+                policies.edges.slice(10, 20).map(profile => profile.node)
+            );
             const instance = wrapper.instance();
             await instance.changePage(2, 10);
-            expect(wrapper.state('currentRows')).toEqual(secondPage);
+            expect(wrapper.state('currentRows')).toEqual(pageTwoPolicies);
             await instance.changePage(1, 10);
-            expect(wrapper.state('currentRows')).toEqual(firstPage);
+            expect(wrapper.state('currentRows')).toEqual(pageOnePolicies);
         });
+
+    });
+
+    it('should lead to the right page when clicking on View latest results', async () => {
+        const policyRows = policies.edges.map(profile => profile.node);
+        const wrapper = shallow(
+            <PoliciesTable
+                history={history}
+                policies={policyRows}
+            />);
+        const instance = wrapper.instance();
+        await instance.setState({ page: 1, perPage: 10 });
+        await instance.actionResolver({ id: 1 })[0].onClick();
+        expect(history.push).toHaveBeenNthCalledWith(1, `/policies/${policyRows[1].id}`);
+        history.push.mockReset();
+        await instance.actionResolver({ id: 4 })[0].onClick();
+        expect(history.push).toHaveBeenNthCalledWith(1, `/policies/${policyRows[4].id}`);
+        history.push.mockReset();
+        await instance.setState({ page: 2, perPage: 10 });
+        await instance.actionResolver({ id: 0 })[0].onClick();
+        expect(history.push).toHaveBeenNthCalledWith(1, `/policies/${policyRows[10].id}`);
+        history.push.mockReset();
+        await instance.actionResolver({ id: 2 })[0].onClick();
+        expect(history.push).toHaveBeenNthCalledWith(1, `/policies/${policyRows[12].id}`);
     });
 });
