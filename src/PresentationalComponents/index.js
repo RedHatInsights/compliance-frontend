4540a487f4905e4bff0be4b4b65b57ab952655fe
Sebastian Gräßl <mail@bastilian.me>
2020-02-18 17:09:06 +0100

    Remove obsolete DownloadsButton and SystmsComplianceFilter components

--- src/PresentationalComponents/index.js
+++ src/PresentationalComponents/index.js
@@ -4,7 +4,6 @@ export { default as LoadingPoliciesTable } from './LoadingPoliciesTable/LoadingP
 export { default as CompliancePolicyCard } from './CompliancePolicyCard/CompliancePolicyCard';
 export { default as CompliancePoliciesEmptyState } from './CompliancePoliciesEmptyState/CompliancePoliciesEmptyState';
 export { default as ErrorPage } from './ErrorPage/ErrorPage';
-export { default as SystemsComplianceFilter } from './SystemsComplianceFilter/SystemsComplianceFilter';
 export { default as PolicyDetailsContentLoader } from './PolicyDetailsContentLoader/PolicyDetailsContentLoader';
 export { default as ProfileTypeSelect } from './ProfileTypeSelect/ProfileTypeSelect';
 export { default as ComplianceScore, complianceScoreString } from './ComplianceScore/ComplianceScore';
--- src/SmartComponents/index.js
+++ src/SmartComponents/index.js
@@ -1,6 +1,5 @@
 export { default as ProfileThresholdField } from './ProfileThresholdField/ProfileThresholdField';
 export { default as BusinessObjectiveField } from './BusinessObjectiveField/BusinessObjectiveField';
-export { default as DownloadTableButton } from './DownloadTableButton/DownloadTableButton';
 export { default as ComplianceSystems } from './ComplianceSystems/ComplianceSystems';
 export { default as AssignPoliciesModal } from './AssignPoliciesModal/AssignPoliciesModal';
 export { default } from './InventoryDetails/InventoryDetails';
