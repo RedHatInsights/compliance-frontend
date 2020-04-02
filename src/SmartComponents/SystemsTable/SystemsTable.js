5c582515e36abdcae12189a4ab855fe7af92039d
Sebastian Gräßl <mail@bastilian.me>
2020-04-02 20:38:04 +0200

    Send update filters to inventory component

--- src/SmartComponents/SystemsTable/SystemsTable.js
+++ src/SmartComponents/SystemsTable/SystemsTable.js
@@ -21,7 +21,6 @@ import  {
 import { exportToCSV } from '../../store/ActionTypes.js';
 import { exportToJson } from 'Utilities/Export';
 import { FilterConfigBuilder } from '@redhat-cloud-services/frontend-components-inventory-compliance';
-import { stringToId } from 'Utilities/TextHelper';
 import { entitiesReducer } from '../../store/Reducers/SystemStore';
 import { FILTER_CONFIGURATION } from '../../constants';
 
