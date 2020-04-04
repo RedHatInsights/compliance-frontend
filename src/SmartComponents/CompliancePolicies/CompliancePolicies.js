f672a8c59757c415a14370d8cce5a9ac3872ae5d
Daniel Lobato Garcia <me@daniellobato.me>
2020-04-04 20:17:45 +0200

    Update cache-settings for Reports page
    The Reports page is not updating properly when you do stuff such as
    removing a policy, or clicking back and forth between the vertical
    navigation elements. Instead, we can use cache-and-network which returns
    the cache elements first - and makes a network call in the backend,
    which is only used to populate the data if it's different to what was in
    the cache.

--- src/SmartComponents/CompliancePolicies/CompliancePolicies.js
+++ src/SmartComponents/CompliancePolicies/CompliancePolicies.js
@@ -32,7 +32,7 @@ const QUERY = gql`
 `;
 
 export const CompliancePolicies = () => {
-    const { data, error, loading, refetch } = useQuery(QUERY, { fetchPolicy: 'network-only' });
+    const { data, error, loading, refetch } = useQuery(QUERY, { fetchPolicy: 'cache-and-network' });
 
     if (error) { return <ErrorPage error={error}/>; }
 
