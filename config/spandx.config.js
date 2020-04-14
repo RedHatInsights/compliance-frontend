67ae7aead1e4a88c9aa4d0a68284b1f7ef6e3325
Andrew Kofink <akofink@redhat.com>
2020-04-14 03:01:43 -0400

    Beta section is different than stable (#501)
    Signed-off-by: Andrew Kofink <akofink@redhat.com>
    
    Co-authored-by: Daniel Lobato García <me@daniellobato.me>

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,12 +1,13 @@
 /*global module*/
 
 const SECTION = 'rhel';
+const BETA_SECTION = 'insights';
 const APP_ID = 'compliance';
 const FRONTEND_PORT = 8002;
 const API_PORT = 3000;
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/beta/${BETA_SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
