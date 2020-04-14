209157d678ff09d0eb2f1313b597ba371d12d77d
Daniel Lobato Garcia <me@daniellobato.me>
2020-04-14 21:50:49 +0200

    RHICOMPL-575 - AssignPoliciesModal only lists policies matching OS

--- src/SmartComponents/AssignPoliciesModal/AssignPoliciesModal.test.js
+++ src/SmartComponents/AssignPoliciesModal/AssignPoliciesModal.test.js
@@ -1,10 +1,23 @@
 import toJson from 'enzyme-to-json';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import { act } from 'react-dom/test-utils';
 import { useQuery, useMutation } from '@apollo/react-hooks';
+
+const waitForComponentToPaint = async (wrapper: any) => {
+    await act(async () => {
+        await new Promise(resolve => setTimeout(resolve, 0));
+        wrapper.update();
+    });
+};
 
 const mockStore = configureStore();
 jest.mock('@apollo/react-hooks');
+jest.mock('../../Utilities/InventoryApi', () => (
+    {
+        getSystemProfile: () => Promise.resolve({ results: [{ system_profile: { os_release: '8.1' } }] })
+    }
+));
 jest.mock('../CreatePolicy/EditPolicyRules', () => {
     return <p>Rules table</p>;
 });
@@ -33,7 +46,9 @@ describe('AssignPoliciesModal', () => {
                 options.onCompleted();
             }];
         });
+    });
 
+    it('expect to render an open modal without error', async () => {
         useQuery.mockImplementation(() => ({
             data: {
                 systems: {
@@ -44,7 +59,8 @@ describe('AssignPoliciesModal', () => {
                             profiles: [
                                 {
                                     id: '1',
-                                    name: 'profile1'
+                                    name: 'profile1',
+                                    majorOsVersion: '8'
                                 }
                             ]
                         }
@@ -53,30 +69,91 @@ describe('AssignPoliciesModal', () => {
                 profiles: {
                     nodes: [{
                         id: '1',
-                        name: 'profile1'
+                        name: 'profile1',
+                        majorOsVersion: '8'
+                    }, {
+                        id: '2',
+                        name: 'profile2',
+                        majorOsVersion: '7'
                     }]
                 }
             }, error: false, loading: false }));
-    });
-
-    it('expect to render an open modal without error', () => {
 
         const component = mount(
             <Provider store={store}>
-                <AssignPoliciesModal { ...defaultProps } isModalOpen={true} />
+                <AssignPoliciesModal { ...defaultProps } isModalOpen={ true } />
             </Provider>
         );
-
+        await waitForComponentToPaint(component);
         expect(toJson(component)).toMatchSnapshot();
     });
 
-    it('expect not to render anything for a closed modal', () => {
+    it('expect not to render anything for a closed modal', async () => {
+        useQuery.mockImplementation(() => ({
+            data: {
+                systems: {
+                    nodes: [
+                        {
+                            id: '1',
+                            fqdn: 'myhost',
+                            profiles: [
+                                {
+                                    id: '1',
+                                    name: 'profile1',
+                                    majorOsVersion: '8'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                profiles: {
+                    nodes: [{
+                        id: '1',
+                        name: 'profile1',
+                        majorOsVersion: '8'
+                    }]
+                }
+            }, error: false, loading: false }));
+
         const component = mount(
             <Provider store={store}>
                 <AssignPoliciesModal { ...defaultProps } isModalOpen={ false } />
             </Provider>
         );
+        await waitForComponentToPaint(component);
+        expect(toJson(component)).toMatchSnapshot();
+    });
 
+    it('renders empty state if no OS is matching the system', async () => {
+        useQuery.mockImplementation(() => ({
+            data: {
+                systems: {
+                    nodes: [
+                        {
+                            id: '1',
+                            fqdn: 'myhost'
+                        }
+                    ]
+                },
+                profiles: {
+                    nodes: [{
+                        id: '1',
+                        name: 'profile1',
+                        majorOsVersion: '7'
+                    }, {
+                        id: '2',
+                        name: 'profile2',
+                        majorOsVersion: '7'
+                    }]
+                }
+            }, error: false, loading: false }));
+
+        const component = mount(
+            <Provider store={store}>
+                <AssignPoliciesModal { ...defaultProps } isModalOpen={ open } />
+            </Provider>
+        );
+        await waitForComponentToPaint(component);
         expect(toJson(component)).toMatchSnapshot();
     });
 });
