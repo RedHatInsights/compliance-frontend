26d8d8e213c57ad96b91cb791ad053f001b67d12
Sebastian Gräßl <mail@bastilian.me>
2020-04-16 22:32:19 +0200

    Add warning about ssg version rules

--- src/SmartComponents/PolicyDetails/PolicyRulesTab.js
+++ src/SmartComponents/PolicyDetails/PolicyRulesTab.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { Alert } from '@patternfly/react-core';
+import { Alert, Text, TextVariants, PageSection, PageSectionVariants } from '@patternfly/react-core';
 import { SystemRulesTable, ANSIBLE_ICON } from '@redhat-cloud-services/frontend-components-inventory-compliance';
 import { sortable } from '@patternfly/react-table';
 
-const PolicyRulesTab = ({ loading, policy }) => (<React.Fragment>
-    <Alert variant="info" isInline title="Rule editing coming soon" />
-    <SystemRulesTable
-        remediationsEnabled={false}
-        columns={[
-            { title: 'Rule', transforms: [sortable] },
-            { title: 'Severity', transforms: [sortable] },
-            { title: <React.Fragment>{ ANSIBLE_ICON } Ansible</React.Fragment>, transforms: [sortable], original: 'Ansible' }
-        ]}
-        loading={ loading }
-        profileRules={[{
-            profile: { refId: policy.refId, name: policy.name },
-            rules: policy.rules
-        }]}
-    />
-</React.Fragment>);
+const PolicyRulesTab = ({ loading, policy }) => (
+    <React.Fragment>
+        <Alert variant="info" isInline title="Rule editing coming soon" />
+        <PageSection variant={PageSectionVariants.light}>
+            <Text component={TextVariants.p}>
+                <strong>What rules are shown on this list?&nbsp;</strong>
+                This view shows rules that are from the latest SSG version ({ policy.benchmark.version }).
+                If you are using a different version of SSG for systems in this policy,
+                those rules will be different and can be viewed on the systems details page.
+            </Text>
+        </PageSection>
+        <SystemRulesTable
+            remediationsEnabled={false}
+            columns={[
+                { title: 'Rule', transforms: [sortable] },
+                { title: 'Severity', transforms: [sortable] },
+                { title: <React.Fragment>{ ANSIBLE_ICON } Ansible</React.Fragment>, transforms: [sortable], original: 'Ansible' }
+            ]}
+            loading={ loading }
+            profileRules={[{
+                profile: { refId: policy.refId, name: policy.name },
+                rules: policy.rules
+            }]}
+        />
+    </React.Fragment>
+);
 
 PolicyRulesTab.propTypes = {
     loading: propTypes.bool,
     policy: propTypes.shape({
         name: propTypes.string,
         refId: propTypes.string,
-        rules: propTypes.array
+        rules: propTypes.array,
+        benchmark: propTypes.object
     })
 };
 
