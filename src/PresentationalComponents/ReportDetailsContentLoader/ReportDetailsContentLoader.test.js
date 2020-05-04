29f25a2cfba81e6440682ddad23adc1b77952ab3
Sebastian Gräßl <mail@bastilian.me>
2020-05-04 09:21:59 +0200

    Add test for ReportDetailsContent

---
+++ src/PresentationalComponents/ReportDetailsContentLoader/ReportDetailsContentLoader.test.js
@@ -0,0 +1,11 @@
+import ReportDetailsContentLoader from './ReportDetailsContentLoader';
+
+describe('ReportDetailsContentLoader', () => {
+    it('expect to render without error', () => {
+        const wrapper = shallow(
+            <ReportDetailsContentLoader />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
