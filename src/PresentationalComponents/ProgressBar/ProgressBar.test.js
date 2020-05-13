478db25fa88f24e31a8c304b50190b42fb4c32f8
Daniel Lobato Garcia <me@daniellobato.me>
2020-05-13 14:09:27 +0200

    Add snaps

---
+++ src/PresentationalComponents/ProgressBar/ProgressBar.test.js
@@ -0,0 +1,28 @@
+import renderer from 'react-test-renderer';
+import ProgressBar from './ProgressBar';
+
+describe('ProgressBar', () => {
+    it('expect to render progress bar without error', () => {
+        const component = renderer.create(
+            <ProgressBar percent={0} />
+        );
+
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+    it('expect to render failed bar without error', () => {
+        const component = renderer.create(
+            <ProgressBar percent={50} failed />
+        );
+
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+    it('expect to render success bar without error', () => {
+        const component = renderer.create(
+            <ProgressBar percent={100} />
+        );
+
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+});
