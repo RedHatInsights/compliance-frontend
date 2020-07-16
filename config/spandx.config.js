9a23836416bf4690506e7234f35106382c91836d
Sebastian Gräßl <mail@bastilian.me>
2020-07-16 18:15:56 +0200

    Only proxy backend when LOCAL_API is set

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -6,14 +6,16 @@ const APP_ID = 'compliance';
 const FRONTEND_PORT = 8002;
 const API_PORT = 3000;
 const routes = {};
-const frontentHost = process.env.UI_HOST ? process.env.UI_HOST : 'frontend'
+const frontendHost = process.env.UI_HOST ? process.env.UI_HOST : 'frontend';
 
-routes[`/beta/${BETA_SECTION}/${APP_ID}`] = { host: `http://${frontentHost}:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `http://${frontentHost}:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://${frontentHost}:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `http://${frontentHost}:${FRONTEND_PORT}` };
+routes[`/beta/${BETA_SECTION}/${APP_ID}`] = { host: `http://${frontendHost}:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `http://${frontendHost}:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `http://${frontendHost}:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `http://${frontendHost}:${FRONTEND_PORT}` };
 
-routes[`/api/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
-routes[`/r/insights/platform/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
+if (process.env.LOCAL_API) {
+    routes[`/api/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
+    routes[`/r/insights/platform/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
+}
 
 module.exports = { routes };
