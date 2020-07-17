9af180d4a9bf8ccc745994cdb0706e7cf5aa486a
Sebastian Gräßl <mail@bastilian.me>
2020-07-17 15:50:13 +0200

    Optionally install packages when none found

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
+
+if [[ ! -d "/frontend/node_modules/@redhat-cloud-services" ]]; then
+    echo "No modules installed! Installing..."
+    cd /frontend; /usr/bin/npm install;
+fi
+
 exec "$@"
