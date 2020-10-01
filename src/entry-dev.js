4713df85bddbdf526e2e15a5876c6e80d6148ee7
Sebastian Gräßl <bastilian@users.noreply.github.com>
2020-10-01 16:22:14 +0200

    chore: Clean up entry files (#744)
    * Remove unnecessary `getUser` call
     * use `getBaseName` from frontend components utils

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import logger from 'redux-logger';
 import { IntlProvider } from 'react-intl';
-
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/files/helpers';
 import { init } from 'Store';
 import App from './App';
 
@@ -17,26 +17,15 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
-const pathName = window.location.pathname.split('/');
-pathName.shift();
-
-let release = '/';
-if (pathName[0] === 'beta') {
-    release = `/${pathName.shift()}/`;
-}
-
-window.insights.chrome.auth.getUser().then(() => {
-    ReactDOM.render(
-        <Provider store={ init(logger).getStore() }>
-            <IntlProvider locale={navigator.language}>
-                <Router basename={`${release}${pathName[0]}/${pathName[1]}`}>
-                    <ApolloProvider client={client}>
-                        <App />
-                    </ApolloProvider>
-                </Router>
-            </IntlProvider>
-        </Provider>,
-
-        document.getElementById('root')
-    );
-});
+ReactDOM.render(
+    <Provider store={ init(logger).getStore() }>
+        <IntlProvider locale={ navigator.language }>
+            <Router basename={ getBaseName(window.location.pathname) }>
+                <ApolloProvider client={ client }>
+                    <App />
+                </ApolloProvider>
+            </Router>
+        </IntlProvider>
+    </Provider>,
+    document.getElementById('root')
+);
