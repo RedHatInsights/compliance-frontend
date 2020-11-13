f8a3640f8a4e24c1282cb9853049e612c9d3e8e4
Viliam Krizan <vkrizan@redhat.com>
2020-11-13 17:06:50 +0100

    fix(RHICOMPL-1105): useFeature to return boolen when set

--- src/Utilities/hooks/useFeature.js
+++ src/Utilities/hooks/useFeature.js
@@ -30,7 +30,7 @@ const setFlagsFromUrl = () => {
 
 // Queries the local storage for feature flag values
 const getLocatStateFlag = (feature) => (
-    localStorage.getItem(`${LOCAL_STORE_FEATURE_PREFIX}:${feature}`)
+    !!localStorage.getItem(`${LOCAL_STORE_FEATURE_PREFIX}:${feature}`)
 );
 
 // A hook to query feature values
