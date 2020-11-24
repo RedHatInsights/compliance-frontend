370c00e032f64d4297bfb78e529ce640f0eba800
Viliam Krizan <vkrizan@redhat.com>
2020-11-24 15:14:51 +0100

    refactor(FinishedCreatePolicy): RHICOMPL-1147 test GraphQL mocks
    fixes the graphql mocks and runs them in the test by resolving

--- src/SmartComponents/CreatePolicy/FinishedCreatePolicy.test.js
+++ src/SmartComponents/CreatePolicy/FinishedCreatePolicy.test.js
@@ -1,6 +1,8 @@
 import FinishedCreatePolicy from './FinishedCreatePolicy.js';
 import configureStore from 'redux-mock-store';
-import { policyFormValues, mutateCreateProfileMock } from '@/__fixtures__/benchmarks_rules.js';
+import {
+    policyFormValues, mutateCreateProfileMock, mutateAssociateSystemsToProfile
+} from '@/__fixtures__/benchmarks_rules.js';
 
 import renderer from 'react-test-renderer';
 import { MockedProvider } from '@apollo/react-testing';
@@ -15,14 +17,18 @@ describe('FinishedCreatePolicy', () => {
         store = mockStore({ form: { policyForm: { values: policyFormValues } } });
     });
 
-    it('expect to render without error', () => {
+    it('expect to render without error', async () => {
         const onClose = () => {};
 
         component = renderer.create(
-            <MockedProvider mocks={[mutateCreateProfileMock]} addTypename={false} >
+            <MockedProvider mocks={[mutateCreateProfileMock, mutateAssociateSystemsToProfile]}
+                addTypename={false} >
                 <FinishedCreatePolicy store={store} onClose={onClose} />
             </MockedProvider>
         );
+
+        await new Promise(resolve => setTimeout(resolve, 0)); // wait for response
+        await new Promise(resolve => setTimeout(resolve, 0)); // wait for response
         expect(component.toJSON()).toMatchSnapshot();
     });
 });
