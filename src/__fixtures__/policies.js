dc1a067f971ae74021adddc3f8117c5c4a480864
Andrew Kofink <akofink@redhat.com>
2020-12-08 18:25:04 -0500

    fix(ReportsTable): RHICOMPL-1134 Fix policyType filter
    This uses the newly available policyType attribute on the profile
    GraphQL API to filter in the same way as operating systems.
    
    It also consolidates some logic with the operating systems filter.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- src/__fixtures__/policies.js
+++ src/__fixtures__/policies.js
@@ -110,6 +110,7 @@ export const policies = {
                 id: '719999b6-d230-4ba5-8dba-7ab3dc6561e0',
                 name: 'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 73',
                 refId: 'xccdf_org.ssgproject.content_profile_pci-dss3',
+                policyType: 'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 73',
                 complianceThreshold: 67,
                 businessObjective: null,
                 totalHostCount: 10,
@@ -179,6 +180,7 @@ export const policies = {
                 id: 'dae0487d-3201-4ee0-af5f-b94cde2af818',
                 name: 'United States Government Configuration Baseline2',
                 refId: 'xccdf_org.ssgproject.content_profile_ospp2',
+                policyType: 'United States Government Configuration Baseline2',
                 complianceThreshold: 100,
                 businessObjective: null,
                 totalHostCount: 10,
@@ -196,6 +198,7 @@ export const policies = {
             node: {
                 id: '20a9d997-62a6-40cc-a5f3-19d466eb975e',
                 name: 'C2S for Red Hat Enterprise Linux 7',
+                policyType: 'C2S for Red Hat Enterprise Linux 7',
                 refId: 'xccdf_org.ssgproject.content_profile_C2S',
                 complianceThreshold: 69.5,
                 businessObjective: null,
@@ -214,6 +217,7 @@ export const policies = {
             node: {
                 id: '6d345bd2-d597-4df8-9bcf-71c41155b42c',
                 name: 'Criminal Justice Information Services (CJIS) Security Policy',
+                policyType: 'Criminal Justice Information Services (CJIS) Security Policy',
                 refId: 'xccdf_org.ssgproject.content_profile_cjis',
                 complianceThreshold: 100,
                 businessObjective: null,
@@ -232,6 +236,7 @@ export const policies = {
             node: {
                 id: 'c8e15347-9c2b-495d-8e54-503c2f9582b6',
                 name: 'Unclassified Information in Non-federal Information Systems and Organizations (NIST 800-171)',
+                policyType: 'Unclassified Information in Non-federal Information Systems and Organizations (NIST 800-171)',
                 refId: 'xccdf_org.ssgproject.content_profile_nist-800-171-cui',
                 complianceThreshold: 100,
                 businessObjective: null,
@@ -250,6 +255,7 @@ export const policies = {
             node: {
                 id: '3c4823a1-2c16-46ae-b2fe-0cebf5a03931',
                 name: 'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 7',
+                policyType: 'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 7',
                 refId: 'xccdf_org.ssgproject.content_profile_pci-dss',
                 complianceThreshold: 100,
                 businessObjective: null,
@@ -268,6 +274,7 @@ export const policies = {
             node: {
                 id: 'f7b7977a-403b-4cd1-ab90-20b6f9a5a359',
                 name: 'Red Hat Corporate Profile for Certified Cloud Providers (RH CCP)',
+                policyType: 'Red Hat Corporate Profile for Certified Cloud Providers (RH CCP)',
                 refId: 'xccdf_org.ssgproject.content_profile_rht-ccp',
                 complianceThreshold: 100,
                 businessObjective: null,
@@ -286,6 +293,7 @@ export const policies = {
             node: {
                 id: '36abc364-6dc3-4e35-94f4-d10fa77e866e',
                 name: 'Health Insurance Portability and Accountability Act (HIPAA)',
+                policyType: 'Health Insurance Portability and Accountability Act (HIPAA)',
                 refId: 'xccdf_org.ssgproject.content_profile_hipaa',
                 complianceThreshold: 100,
                 totalHostCount: 10,
@@ -304,6 +312,7 @@ export const policies = {
             node: {
                 id: 'd35c8aad-8fc8-49e8-bff0-4d9d3dc8f220',
                 name: 'United States Government Configuration Baseline',
+                policyType: 'United States Government Configuration Baseline',
                 refId: 'xccdf_org.ssgproject.content_profile_ospp',
                 complianceThreshold: 100,
                 businessObjective: null,
@@ -322,6 +331,7 @@ export const policies = {
             node: {
                 id: '4c27fe09-9a7f-437c-b38b-e42272d9ccf0',
                 name: 'Standard System Security Profile for Red Hat Enterprise Linux 7',
+                policyType: 'Standard System Security Profile for Red Hat Enterprise Linux 7',
                 refId: 'xccdf_org.ssgproject.content_profile_standard',
                 complianceThreshold: 100,
                 businessObjective: null,
@@ -340,6 +350,7 @@ export const policies = {
             node: {
                 id: '9b034440-e3dd-4c19-8f2c-ca75e813d57d',
                 name: 'DISA STIG for Red Hat Enterprise Linux 7',
+                policyType: 'DISA STIG for Red Hat Enterprise Linux 7',
                 refId: 'xccdf_org.ssgproject.content_profile_stig-rhel7-disa',
                 complianceThreshold: 100,
                 businessObjective: null,
@@ -358,6 +369,7 @@ export const policies = {
             node: {
                 id: '19921ca4-8526-4651-8876-3c8587e8e125',
                 name: 'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 72',
+                policyType: 'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 72',
                 refId: 'xccdf_org.ssgproject.content_profile_pci-dss2',
                 complianceThreshold: 100,
                 businessObjective: null,
