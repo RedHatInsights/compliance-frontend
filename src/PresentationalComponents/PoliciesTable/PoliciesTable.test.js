edca33b7e0736b271e90afe8e9be0439285cd894
Sebastian Gräßl <mail@bastilian.me>
2020-12-17 03:45:37 +0100

    fix(PoliciesTable): Pass history and location mock props in tests

--- src/PresentationalComponents/PoliciesTable/PoliciesTable.test.js
+++ src/PresentationalComponents/PoliciesTable/PoliciesTable.test.js
@@ -5,12 +5,18 @@ import { PoliciesTable, PolicyNameCell } from './PoliciesTable.js';
 const policies = rawPolicies.edges.map(profile => profile.node);
 
 describe('PoliciesTable', () => {
+    const defaultProps = {
+        history: { push: jest.fn() },
+        location: {}
+    };
     let wrapper;
     let instance;
 
     it('expect to render without error', () => {
         wrapper = shallow(
-            <PoliciesTable policies={ policies } />
+            <PoliciesTable
+                { ...defaultProps }
+                policies={ policies } />
         );
 
         expect(toJson(wrapper)).toMatchSnapshot();
@@ -18,7 +24,9 @@ describe('PoliciesTable', () => {
 
     it('expect to render emptystate', () => {
         wrapper = shallow(
-            <PoliciesTable policies={[]} />
+            <PoliciesTable
+                { ...defaultProps }
+                policies={[]} />
         );
 
         expect(toJson(wrapper)).toMatchSnapshot();
@@ -28,6 +36,7 @@ describe('PoliciesTable', () => {
         beforeEach(() => {
             wrapper = shallow(
                 <PoliciesTable
+                    { ...defaultProps }
                     policies={ policies }
                 />);
             instance = wrapper.instance();
