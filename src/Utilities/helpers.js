1bff95ba7dd88570af7566153a23123834d68445
Viliam Krizan <vkrizan@redhat.com>
2021-03-16 12:46:17 +0100

    feat(utils): RHICOMPL-1521 sortingByProp

--- src/Utilities/helpers.js
+++ src/Utilities/helpers.js
@@ -1,1 +1,15 @@
 export const uniq = (collection) => [...new Set(collection)];
+
+export const sortingByProp = (propName, order = 'asc') => (
+    (objA, objB) => {
+        const propA = (objA && objA[propName]) || '';
+        const propB = (objB && objB[propName]) || '';
+        if (propA === propB) {
+            return 0;
+        } else if (order === 'asc') {
+            return propA < propB ? -1 : 1;
+        } else {
+            return propA < propB ? 1 : -1;
+        }
+    }
+);
