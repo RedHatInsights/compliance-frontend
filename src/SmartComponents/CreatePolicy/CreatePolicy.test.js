85116c0a37d6c2206fe7f556c4e0243e91ecb693
Dávid Halász <dhalasz@redhat.com>
2021-03-23 11:33:15 +0100

    feat(CreatePolicy): RHICOMPL-1548 validate >0 systems selection

--- src/SmartComponents/CreatePolicy/CreatePolicy.test.js
+++ src/SmartComponents/CreatePolicy/CreatePolicy.test.js
@@ -17,4 +17,16 @@ describe('CreatePolicy', () => {
         );
         expect(toJson(wrapper.find('Wizard'))).toMatchSnapshot();
     });
+
+    it('expect to render the wizard with enableNext on systems', () => {
+        useQuery.mockImplementation(() => ({
+            data: { latestBenchmarks: benchmarksQuery },
+            error: false,
+            loading: false
+        }));
+        const wrapper = shallow(
+            <CreatePolicy systemIds={['123', '456']}/>
+        );
+        expect(wrapper.find('Wizard').prop('steps').find(({ name }) => name === 'Systems').enableNext).toMatchSnapshot();
+    });
 });
