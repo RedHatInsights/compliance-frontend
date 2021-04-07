671498f446b511a4ff512bfa0b22d1e6a53eb798
Viliam Krizan <vkrizan@redhat.com>
2021-04-07 17:52:24 +0200

    feat(Rules): RHICOMPL-1551 add SSG popover to tab content

--- src/PresentationalComponents/TabbedRules/ProfileTabContent.js
+++ src/PresentationalComponents/TabbedRules/ProfileTabContent.js
@@ -3,10 +3,11 @@ import propTypes from 'prop-types';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import {
-    Text, TextVariants, TextContent, Grid, Spinner, Badge
+    Text, TextVariants, TextContent, Grid, Spinner, Badge, Popover
 } from '@patternfly/react-core';
+import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
 import SystemRulesTable from '@redhat-cloud-services/frontend-components-inventory-compliance/SystemRulesTable';
-import { StateViewWithError, StateViewPart } from 'PresentationalComponents';
+import { StateViewWithError, StateViewPart, SupportedSSGVersionsLink } from 'PresentationalComponents';
 import { pluralize } from 'Utilities/TextHelper';
 import OsVersionText from './OsVersionText';
 
@@ -19,6 +20,43 @@ const ProfileSystemCount = ({ count = 0 }) => (
 ProfileSystemCount.propTypes = {
     profile: propTypes.object,
     count: propTypes.number
+};
+
+const SSGVersionText = ({ profile, newOsMinorVersion }) => (
+    <Text component={ TextVariants.p }>
+        SSG version { profile.ssgVersion }
+        {' '}
+        <Popover
+            position='right'
+            bodyContent={ <SSGPopoverBody { ...{ profile, newOsMinorVersion } } /> }
+            footerContent={ <SupportedSSGVersionsLink /> }>
+            <OutlinedQuestionCircleIcon style={ { cursor: 'pointer' } } />
+        </Popover>
+    </Text>
+);
+
+SSGVersionText.propTypes = {
+    profile: propTypes.object.isRequired,
+    newOsMinorVersion: propTypes.string
+};
+
+const SSGPopoverBody = ({ profile, newOsMinorVersion }) => (
+    <TextContent style={ { fontSize: 'var(--pf-c-popover--FontSize)' } }>
+        <Text>
+            This is the latest supported version of the SCAP Security Guide (SSG) for
+            {' '}
+            <OsVersionText { ...{ profile, newOsMinorVersion } } />
+        </Text>
+        <Text>
+            <OsVersionText { ...{ profile, newOsMinorVersion } } /> systems assigned to this
+            policy will report using this rule list.
+        </Text>
+    </TextContent>
+);
+
+SSGPopoverBody.propTypes = {
+    profile: propTypes.object.isRequired,
+    newOsMinorVersion: propTypes.string
 };
 
 const BENCHMARK_QUERY = gql`
@@ -55,13 +93,11 @@ const ProfileTabContent = ({
         <Grid>
             <TextContent className="pf-u-mt-md">
                 <Text component={ TextVariants.h3 } >
-                    <OsVersionText profile={ profile } newOsMinorVersion={ newOsMinorVersion} />
+                    <OsVersionText { ...{ profile, newOsMinorVersion } } />
                     {' '}
                     <ProfileSystemCount count={ systemCount } />
                 </Text>
-                <Text component={ TextVariants.p }>
-                    SSG version { profile.ssgVersion }
-                </Text>
+                <SSGVersionText { ...{ profile, newOsMinorVersion } } />
             </TextContent>
         </Grid>
         <StateViewWithError stateValues={ { error, loading, rules } }>
