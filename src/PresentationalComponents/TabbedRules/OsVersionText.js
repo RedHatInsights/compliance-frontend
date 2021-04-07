883da49736c2d7cf9fb717a7452337be73c2c10e
Viliam Krizan <vkrizan@redhat.com>
2021-04-07 17:52:24 +0200

    feat(Rules): RHICOMPL-1551 add SSG popover to tab content

--- src/PresentationalComponents/TabbedRules/OsVersionText.js
+++ src/PresentationalComponents/TabbedRules/OsVersionText.js
@@ -1,5 +1,5 @@
 const OsVersionText = ({ profile, newOsMinorVersion }) => (
-    `RHEL ${ profile.osMajorVersion }.${ (profile.osMinorVersion || newOsMinorVersion) }`
+    `RHEL${'\u00A0'}${ profile.osMajorVersion }.${ (profile.osMinorVersion || newOsMinorVersion) }`
 );
 
 export default OsVersionText;
