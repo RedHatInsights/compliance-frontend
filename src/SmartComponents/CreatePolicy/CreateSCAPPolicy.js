9c32b95d5e5584a716f036fda1860358f112700c
Dávid Halász <dhalasz@redhat.com>
2021-04-15 13:59:12 +0200

    fix(PolicyCreate): RHICOMPL-1661 add missing tooltip

--- src/SmartComponents/CreatePolicy/CreateSCAPPolicy.js
+++ src/SmartComponents/CreatePolicy/CreateSCAPPolicy.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
 import {
     Button,
     Form,
     FormGroup,
     Text,
     TextContent,
-    TextVariants
+    TextVariants,
+    Tooltip
 } from '@patternfly/react-core';
 import { ProfileTypeSelect } from 'PresentationalComponents';
 import gql from 'graphql-tag';
@@ -46,7 +48,16 @@ query benchmarksAndProfiles {
 }
 `;
 
-const CreateSCAPPolicy = ({ change, selectedBenchmarkId }) => {
+const PolicyTooltip = () => (
+    <Tooltip
+        position="right"
+        content="Policy types are OpenSCAP policies that are supported by RHEL.
+        For each major version of RHEL, users can create one policy of each type.">
+        <OutlinedQuestionCircleIcon style={{ opacity: 0.5 }}/>
+    </Tooltip>
+);
+
+export const CreateSCAPPolicy = ({ change, selectedBenchmarkId }) => {
     const { data, error, loading } = useQuery(BENCHMARKS_AND_PROFILES, { fetchPolicy: 'no-cache' });
 
     const inUseProfileRefIds = (profiles, benchmark) => (
@@ -110,7 +121,11 @@ const CreateSCAPPolicy = ({ change, selectedBenchmarkId }) => {
                         );
                     })}
                 </FormGroup>
-                <FormGroup label="Policy type" isRequired fieldId="policy-type">
+                <FormGroup
+                    isRequired
+                    labelIcon={<PolicyTooltip/>}
+                    label="Policy type"
+                    fieldId="policy-type">
                     <ProfileTypeSelect
                         profiles={selectedBenchmark && validProfiles }
                         onClick={ () => {
