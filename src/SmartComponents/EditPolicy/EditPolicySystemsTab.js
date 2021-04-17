15652461d8a013ae74397d855f611ff9938030d4
Sebastian Gräßl <mail@bastilian.me>
2021-04-17 04:01:23 +0200

    refactor: Move components out of parent function body

--- src/SmartComponents/EditPolicy/EditPolicySystemsTab.js
+++ src/SmartComponents/EditPolicy/EditPolicySystemsTab.js
@@ -6,6 +6,40 @@ import { GET_SYSTEMS_WITHOUT_FAILED_RULES } from '../SystemsTable/constants';
 import propTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 
+const EmptyState = ({ osMajorVersion }) => (
+    <React.Fragment>
+        <TextContent className="pf-u-mb-md">
+            <Text>
+                You do not have any <b>RHEL { osMajorVersion }</b> systems connected to Insights and enabled for Compliance.
+            </Text>
+        </TextContent>
+        <TextContent className="pf-u-mb-md">
+            <Text>
+                Connect RHEL { osMajorVersion } systems to Insights.
+            </Text>
+        </TextContent>
+    </React.Fragment>
+);
+
+EmptyState.propTypes = {
+    osMajorVersion: propTypes.number
+};
+
+const PrependComponent = ({ osMajorVersion }) => (
+    <React.Fragment>
+        <TextContent className="pf-u-mb-md">
+            <Text>
+                Select which of your <b>RHEL { osMajorVersion }</b> systems should be included
+                in this policy.
+            </Text>
+        </TextContent>
+    </React.Fragment>
+);
+
+PrependComponent.propTypes = {
+    osMajorVersion: propTypes.number
+};
+
 const EditPolicySystemsTab = ({ osMajorVersion, policyOsMinorVersions }) => {
     const { push, location } = useHistory();
     const selectedSystemOsMinorVersions = useSelector(state => (
@@ -34,34 +68,12 @@ const EditPolicySystemsTab = ({ osMajorVersion, policyOsMinorVersions }) => {
         renderFunc: (osMinorVersion, _id, { osMajorVersion }) => `RHEL ${osMajorVersion}.${osMinorVersion}`
     }];
 
-    const emptyStateComponent = (<React.Fragment>
-        <TextContent className="pf-u-mb-md">
-            <Text>
-                You do not have any <b>RHEL { osMajorVersion }</b> systems connected to Insights and enabled for Compliance.
-            </Text>
-        </TextContent>
-        <TextContent className="pf-u-mb-md">
-            <Text>
-                Connect RHEL { osMajorVersion } systems to Insights.
-            </Text>
-        </TextContent>
-    </React.Fragment>);
-
-    const prependComponent = (<React.Fragment>
-        <TextContent className="pf-u-mb-md">
-            <Text>
-                Select which of your <b>RHEL { osMajorVersion }</b> systems should be included
-                in this policy.
-            </Text>
-        </TextContent>
-    </React.Fragment>);
-
     return (
         <React.Fragment>
             <InventoryTable
                 showOsMinorVersionFilter={ [osMajorVersion] }
-                prependComponent={ prependComponent }
-                emptyStateComponent={ emptyStateComponent }
+                prependComponent={ <PrependComponent osMajorVersion={ osMajorVersion } />  }
+                emptyStateComponent={ <EmptyState osMajorVersion={ osMajorVersion } />  }
                 columns={ columns }
                 compact
                 showActions={ false }
