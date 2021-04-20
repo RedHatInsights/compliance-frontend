a408cff556f1a2cad8935f01fee9fa975287d745
Sebastian Gräßl <mail@bastilian.me>
2021-04-20 14:05:29 +0200

    chore(features): RHICOMPL-1723 - Remove unused reducer code

--- src/store/Reducers/SystemStore.js
+++ src/store/Reducers/SystemStore.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { applyReducerHash } from '@redhat-cloud-services/frontend-components-utilities/ReducerRegistry';
-import DateFormat from '@redhat-cloud-services/frontend-components/DateFormat';
 import { Link } from 'react-router-dom';
-import { EXPORT, SELECT_ENTITY } from 'Store/ActionTypes';
-import { exportFromState } from 'Utilities/Export';
-import {
-    ComplianceScore as complianceScore,
-    complianceScoreString
-} from 'PresentationalComponents';
+import { SELECT_ENTITY } from 'Store/ActionTypes';
 import {
     Tooltip,
     Text,
@@ -85,17 +79,6 @@ export const hasOsInfo = (matchingSystem) => (
         !(matchingSystem.osMajorVersion === 0 && matchingSystem.osMinorVersion === 0)
 );
 
-const displayNameCell = (system, matchingSystem) =>  ({
-    title: <TextContent>
-        { matchingSystem.name ? <Link to={{ pathname: `/systems/${matchingSystem.id}` }}>
-            { matchingSystem.name }
-        </Link> : system.display_name }
-        { hasOsInfo(matchingSystem) &&
-            <Text component={TextVariants.small}>RHEL {matchingSystem.osMajorVersion}.{matchingSystem.osMinorVersion}</Text> }
-    </TextContent>,
-    exportValue: system.display_name || matchingSystem.name
-});
-
 export const systemName = (displayName, id, { osMajorVersion, osMinorVersion, name }) => (
     <TextContent>
         <Link to={{ pathname: `/systems/${id}` }}>
@@ -128,85 +111,6 @@ export const mapCountOsMinorVersions = (systems) => {
 
 export const countOsMinorVersions = (systems) => (
     Object.values(mapCountOsMinorVersions(systems)).sort(sortingByProp('osMinorVersion', 'desc'))
-);
-
-export const systemsToInventoryEntities = (systems, entities, showAllSystems, selectedEntities) => (
-    entities.map(entity => {
-        // This should compare the inventory ID instead with
-        // the ID in compliance
-        if (entity.facts === undefined) { entity.facts = {}; }
-
-        let matchingSystem = systems.map((s) => s.node).find((system) => {
-            return entity.id === system.id;
-        });
-        if (matchingSystem === undefined) {
-            if (!showAllSystems) { return; }
-
-            matchingSystem = { testResultProfiles: [], policies: [] };
-        }
-
-        matchingSystem.policyNames = policyNames(matchingSystem);
-        matchingSystem.rulesPassed = profilesRulesPassed(matchingSystem.testResultProfiles).length;
-        matchingSystem.rulesFailed = profilesRulesFailed(matchingSystem.testResultProfiles).length;
-        matchingSystem.lastScanned = lastScanned(matchingSystem);
-        matchingSystem.compliant = compliant(matchingSystem);
-        matchingSystem.score = score(matchingSystem);
-        matchingSystem.supported = supported(matchingSystem);
-
-        return {
-            /* eslint-disable camelcase */
-            id: entity.id,
-            selected: isSelected(entity.id, selectedEntities),
-            account: entity.account,
-            bios_uuid: entity.bios_uuid,
-            created: entity.created,
-            display_name: entity.display_name || matchingSystem.name,
-            fqdn: entity.fqdn,
-            insights_id: entity.insights_id,
-            ip_addresses: entity.ip_addresses,
-            mac_addresses: entity.mac_addresses,
-            rhel_machine_id: entity.rhel_machine_id,
-            satellite_id: entity.satellite_id,
-            subscription_manager_id: entity.subscription_manager_id,
-            tags: entity.tags,
-            updated: entity.updated,
-            facts: {
-                inventory: {
-                    hostname: (entity.facts.inventory !== undefined) ?
-                        entity.facts.inventory.hostname :
-                        entity.facts.hostname,
-                    machine_id: (entity.facts.inventory !== undefined) ?
-                        entity.facts.inventory.machine_id :
-                        entity.facts.machine_id,
-                    release: (entity.facts.inventory !== undefined) ?
-                        entity.facts.inventory.release :
-                        entity.facts.release
-                },
-                compliance: {
-                    display_name: displayNameCell(entity, matchingSystem),
-                    policies: policiesCell(matchingSystem),
-                    details_link: detailsLink(matchingSystem),
-                    rules_passed: matchingSystem.rulesPassed,
-                    rules_failed: { title: <Link to={{
-                        pathname: `/systems/${matchingSystem.id}`,
-                        query: {
-                            hidePassed: true
-                        }
-                    }}>{matchingSystem.rulesFailed}</Link> },
-                    rules_failed_text: matchingSystem.rulesFailed,
-                    compliance_score: complianceScore(matchingSystem),
-                    compliance_score_text: complianceScoreString(matchingSystem),
-                    last_scanned: (matchingSystem.lastScanned instanceof Date) ?
-                        { title: <DateFormat date={Date.parse(matchingSystem.lastScanned)} type='relative' /> } :
-                        matchingSystem.lastScanned,
-                    last_scanned_text: matchingSystem.lastScanned,
-                    ssg_version: profilesSsgVersions(matchingSystem),
-                    supported: matchingSystem.supported
-                }
-            }
-            /* eslint-enable camelcase */
-        };
-    }).filter((value) => (!!value))
 );
 
 const systemsToRows = (systems) => (
@@ -314,56 +218,3 @@ export const systemsReducer = (INVENTORY_ACTION, columns) => applyReducerHash({
         selectedEntities: ids
     })
 });
-
-export const entitiesReducer = (INVENTORY_ACTION, columns, showAllSystems) => applyReducerHash(
-    {
-        ['UPDATE_SYSTEMS']: (state, { systems, systemsCount }) => ({
-            ...state,
-            systems,
-            systemsCount
-        }),
-        ['UPDATE_ROWS']: (state) => ({
-            ...state,
-            loaded: true,
-            rows: systemsToInventoryEntities(
-                state.systems || [],
-                state.rows || [],
-                showAllSystems,
-                state.selectedEntities
-            )
-        }),
-        [INVENTORY_ACTION.LOAD_ENTITIES_FULFILLED]: (state) => ({
-            ...state,
-            rows: systemsToInventoryEntities(
-                state.systems || [],
-                state.rows,
-                showAllSystems,
-                state.selectedEntities
-            ),
-            total: !showAllSystems ? state.systemsCount : state.total,
-            columns: state.total > 0 ? columns : [{ title: '' }]
-        }),
-        [EXPORT]: (state, { payload: { format } }) => {
-            exportFromState(state, format);
-            return state;
-        },
-        [SELECT_ENTITY]: (state, { payload: { id, selected, clearAll } }) => {
-            let newState;
-
-            if (id === 0) {
-                newState = selected ? selectAllRows(state) : deselectAllRows(state);
-            } else {
-                newState = selected ? selectRow(state, id) : deselectRow(state, id);
-            }
-
-            if (newState.selectedEntities.length === 0 || clearAll) {
-                newState.selectedEntities = undefined;
-            }
-
-            return newState;
-        },
-        ['SELECT_ENTITIES']: (state, { payload: { ids } }) => ({
-            selectedEntities: ids
-        })
-    }
-);
--- src/store/Reducers/SystemStore.test.js
+++ src/store/Reducers/SystemStore.test.js
@@ -4,49 +4,8 @@ import {
     lastScanned,
     policyNames,
     mapCountOsMinorVersions,
-    countOsMinorVersions,
-    systemsToInventoryEntities
+    countOsMinorVersions
 } from './SystemStore';
-import { systems, entities } from '@/__fixtures__/systems.js';
-
-describe('mapping systems to inventory entities', () => {
-    it('should return an empty set if there are no systems', () => {
-        expect(systemsToInventoryEntities([], entities, false)).toEqual([]);
-    });
-
-    it('should return all systems if showAllSystems is true', () => {
-        expect(systemsToInventoryEntities([], entities, true).length).toEqual(entities.length);
-    });
-
-    it('should only return systems with a matching inventory entity', () => {
-        const oneMatchingEntityInventory = [{ id: 'd5bc2459-21ce-4d11-bc0b-03ea7513dfa6', facts: [] }];
-        const inventoryEntities = systemsToInventoryEntities(systems, oneMatchingEntityInventory);
-        expect(inventoryEntities.length).toBe(1);
-        expect(inventoryEntities).toMatchSnapshot();
-    });
-
-    it('should only return all systems if all have a matching in the inventory for showAllSystems=false', () => {
-        const inventoryEntities = systemsToInventoryEntities(systems, entities, false);
-        const systemIds = systems.map(system => system.node.id);
-        const systemNames = systems.map(system => system.node.name);
-        const systemComplianceScores = [' 10%', ' N/A'];
-        expect(inventoryEntities.length).toBe(2);
-        expect(inventoryEntities.map(entity => entity.id).sort()).toEqual(systemIds.sort());
-        expect(inventoryEntities.map(entity => entity.display_name).sort()).toEqual(systemNames.sort());
-        expect(inventoryEntities.map(entity => entity.facts.compliance.compliance_score_text).sort()).
-        toEqual(systemComplianceScores.sort());
-    });
-
-    it('should always return all systems for showAllSystems=true', () => {
-        const inventoryEntities = systemsToInventoryEntities(systems, entities, true);
-        const systemComplianceScores = [' 10%', ' N/A', ' N/A', ' N/A'];
-        expect(inventoryEntities.length).toBe(entities.length);
-        expect(inventoryEntities.map(entity => entity.id).sort()).toEqual(entities.map(e => e.id).sort());
-        expect(inventoryEntities.map(entity => entity.display_name).sort()).toEqual(entities.map(e => e.display_name).sort());
-        expect(inventoryEntities.map(entity => entity.facts.compliance.compliance_score_text).sort()).
-        toEqual(systemComplianceScores.sort());
-    });
-});
 
 describe('.lastScanned', () => {
     it('should find the latest scan date', () => {
