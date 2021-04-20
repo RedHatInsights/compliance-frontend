8e5dbe3460c0c592d2efe90a879807e2a79e8aa7
Sebastian Gräßl <mail@bastilian.me>
2021-04-20 16:21:18 +0200

    chore(features): RHICOMPL-1723 - Add tests for EditPolicyRulesTab with policy

--- src/SmartComponents/EditPolicy/EditPolicyRulesTab.test.js
+++ src/SmartComponents/EditPolicy/EditPolicyRulesTab.test.js
@@ -1,7 +1,9 @@
 import { useQuery } from '@apollo/client';
 import EditPolicyRulesTab from './EditPolicyRulesTab.js';
+import { policies } from '@/__fixtures__/policies';
 
 jest.mock('@apollo/client');
+const policy = policies.edges[0].node;
 
 describe('EditPolicyRulesTab', () => {
     useQuery.mockImplementation(() => ({
@@ -10,19 +12,35 @@ describe('EditPolicyRulesTab', () => {
                 edges: [{
                     id: '1',
                     osMajorVersion: '7',
-                    rules: []
+                    rules: policy.rules
                 }]
             }
         }, error: undefined, loading: undefined
     }));
 
-    it('expect to render without error', async () => {
+    it('expect to render without error', () => {
         const wrapper = shallow(
             <EditPolicyRulesTab
                 handleSelect={ () => {} }
-                policy={ { policy: { profiles: [] } } }
+                policy={ policies.edges[0].node }
                 selectedRuleRefIds={ [] }
                 osMinorVersionCounts={ {} }
+            />
+        );
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('expect to render with policy passed', () => {
+        const wrapper = shallow(
+            <EditPolicyRulesTab
+                handleSelect={ () => {} }
+                policy={ policies.edges[0].node }
+                selectedRuleRefIds={ [] }
+                osMinorVersionCounts={ {
+                    9: {
+                        osMinorVersion: 9, count: 1
+                    }
+                } }
             />
         );
         expect(toJson(wrapper)).toMatchSnapshot();
