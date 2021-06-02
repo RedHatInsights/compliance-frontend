418309ecfb77a8c81898304e759ee73fdd72adaf
Sebastian Gräßl <mail@bastilian.me>
2021-06-02 21:51:44 +0200

    feat(ProfileSelect): RHICOMPL-1310 - Set eye-catching color for "In use" Label

--- src/PresentationalComponents/ProfileTypeSelect/ProfileTypeSelect.test.js
+++ src/PresentationalComponents/ProfileTypeSelect/ProfileTypeSelect.test.js
@@ -3,7 +3,8 @@ import ProfileTypeSelect from './ProfileTypeSelect';
 describe('ProfileTypeSelect', () => {
     const defaultProps = {
         profiles: [
-            { description: 'foodesc', name: 'fooname', id: 'fooid' }
+            { description: 'foodesc', name: 'fooname', id: 'fooid' },
+            { description: 'foodesc', name: 'fooname', id: 'fooid', disabled: true }
         ]
     };
 
