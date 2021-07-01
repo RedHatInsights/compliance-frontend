7475f2962bb34795b9b354bba0df4634368cab3d
Sebastian Gräßl <mail@bastilian.me>
2021-07-01 14:04:55 +0200

    fix(Policies): RHICOMPL-1818- Add notifications for policy actions

---
+++ src/SmartComponents/EditPolicy/hooks.js
@@ -0,0 +1,41 @@
+import { useCallback, useState } from 'react';
+import { usePolicy } from 'Mutations';
+import { useLinkToBackground, useAnchor } from 'Utilities/Router';
+import { dispatchNotification } from 'Utilities/Dispatcher';
+
+export const useLinkToPolicy = () => {
+    const anchor = useAnchor();
+    const linkToBackground = useLinkToBackground('/scappolicies');
+    return () => {
+        linkToBackground({ hash: anchor });
+    };
+};
+
+export const useOnSave = (policy, updatedPolicyHostsAndRules) => {
+    const updatePolicy = usePolicy();
+    const linkToPolicy = useLinkToPolicy();
+    const [isSaving, setIsSaving] = useState(false);
+    const onSave = useCallback(() => {
+        if (isSaving) { return Promise.resolve({}); }
+
+        setIsSaving(true);
+        updatePolicy(policy, updatedPolicyHostsAndRules).then(() => {
+            setIsSaving(false);
+            dispatchNotification({
+                variant: 'success',
+                title: 'Policy successfully updated'
+            });
+            linkToPolicy();
+        }).catch((error) => {
+            setIsSaving(false);
+            dispatchNotification({
+                variant: 'danger',
+                title: 'Error updating policy',
+                description: error.message
+            });
+            linkToPolicy();
+        });
+    }, []);
+
+    return [isSaving, onSave];
+};
