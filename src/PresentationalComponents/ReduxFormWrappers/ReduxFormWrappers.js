727850c52f4bc834fffb926f1332a87dc29cfabb
Sebastian Gräßl <bastilian@users.noreply.github.com>
2021-11-02 14:32:48 +0100

    chore(deps): Cleanup & update dependencies (#1441)
    * chore(deps): Remove Select form wrapper and dependencies
    
    This isn't used anywhere in compliance.
    
    * chore(deps): Bump remove unused and bump outdated deps

--- src/PresentationalComponents/ReduxFormWrappers/ReduxFormWrappers.js
+++ src/PresentationalComponents/ReduxFormWrappers/ReduxFormWrappers.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { rawComponents } from '@data-driven-forms/pf4-component-mapper';
 import { TextInput, TextArea } from '@patternfly/react-core';
 
 export const ReduxFormTextInput = (field) => (
@@ -13,15 +12,6 @@ export const ReduxFormTextInput = (field) => (
 export const ReduxFormTextArea = (field) => (
   <TextArea
     value={field.input.value || field.defaultValue}
-    onChange={field.input.onChange}
-    {...field}
-  />
-);
-
-export const ReduxFormCreatableSelectInput = (field) => (
-  <rawComponents.RawSelect
-    selectVariant="createable"
-    value={field.selected}
     onChange={field.input.onChange}
     {...field}
   />
