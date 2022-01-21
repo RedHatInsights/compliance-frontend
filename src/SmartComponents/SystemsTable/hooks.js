9e2c18d39a6c198cc5b1df5184dbc103f21ce2b3
Sebastian Gräßl <mail@bastilian.me>
2022-01-21 08:02:57 +0100

    fix(SystemsTable): RHICOMPL-2608 - Disable default OS inventory filter

--- src/SmartComponents/SystemsTable/hooks.js
+++ src/SmartComponents/SystemsTable/hooks.js
@@ -442,6 +442,7 @@ export const useTags = (fetchArguments) => {
         name: true,
         tags: false,
         registeredWith: true,
+        operatingSystem: true,
         stale: true,
       },
       showTags: true,
