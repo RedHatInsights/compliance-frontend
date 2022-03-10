92381dc37c6e21a5e6894198a8733480b9d8d996
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2022-03-10 17:34:43 -0500

    fix(ReportedSystemRow): RHICOMPL-2745 Add dark boarders
    Sign in with an account that has reports.
    Select a report that has at least one system that never reported.
    
    Before, this row would have left and right borders that are the same color as the
    background. With this fix, the left and right borders should be the same
    color as the top and bottom borders of the row.

--- src/SmartComponents/ReportDetails/Components/ReportedSystemRow.js
+++ src/SmartComponents/ReportDetails/Components/ReportedSystemRow.js
@@ -5,7 +5,15 @@ import { RowWrapper } from '@patternfly/react-table';
 const ReportedSystemRow = ({ row, children }) => (
   <RowWrapper
     style={
-      row.testResultProfiles?.length === 0 ? { background: '#F0F0F0' } : {}
+      row.testResultProfiles?.length === 0
+        ? {
+            background: '#F0F0F0',
+            borderLeft:
+              'var(--pf-c-table--border-width--base) solid var(--pf-c-table--BorderColor)',
+            borderRight:
+              'var(--pf-c-table--border-width--base) solid var(--pf-c-table--BorderColor)',
+          }
+        : {}
     }
   >
     {children}
