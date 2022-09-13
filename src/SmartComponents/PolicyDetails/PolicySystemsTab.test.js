12a61e47e9d079767d78a5de5f24badf20bb0ed9
Aleksandr Voznesenskii <avoznese@redhat.com>
2022-09-13 17:34:22 +0200

    feat(PolicyDetails): Fixed failing tests

--- src/SmartComponents/PolicyDetails/PolicySystemsTab.test.js
+++ src/SmartComponents/PolicyDetails/PolicySystemsTab.test.js
@@ -2,6 +2,16 @@ import PolicySystemsTab from './PolicySystemsTab';
 import { policies } from '@/__fixtures__/policies';
 import useFeature from 'Utilities/hooks/useFeature';
 jest.mock('Utilities/hooks/useFeature');
+jest.mock('react-router-dom', () => ({
+  __esModule: true,
+  useLocation: jest.fn().mockReturnValue({
+    pathname: '/another-route',
+    search: '',
+    hash: '',
+    state: null,
+    key: '5nvxpbdafa',
+  }),
+}));
 
 describe('PolicySystemsTab', () => {
   it('expect to render without error', () => {
