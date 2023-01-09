8e26d6d8b1bb3787191c63c82b18b7b490f7f2e6
Aleksandr Voznesenskii <avoznese@redhat.com>
2023-01-09 16:55:20 +0100

    feat(test): add cypress tests

--- babel.config.js
+++ babel.config.js
@@ -1,57 +1,66 @@
 const IconMapper = {
-    AnsibeTowerIcon: 'ansibeTower-icon'
+  AnsibeTowerIcon: 'ansibeTower-icon',
 };
 
 module.exports = {
-    presets: [
-        [
-            '@babel/env',
-            {
-                targets: '> 0.25%, not dead'
-            }
-        ],
-        '@babel/react'
+  env: {
+    componentTest: {
+      plugins: ['istanbul'],
+    },
+  },
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: '> 0.25%, not dead',
+      },
     ],
-    plugins: [
-        [
-            '@babel/plugin-proposal-decorators',
-            {
-                legacy: true
-            }
-        ],
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-object-rest-spread',
-        'babel-plugin-lodash',
-        '@babel/plugin-transform-react-display-name',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-optional-chaining',
-        [
-            'transform-imports', {
-                '@patternfly/react-icons': {
-                    transform: (importName) => (
-                        `@patternfly/react-icons/dist/esm/icons/${IconMapper[importName] || importName
-                        .split(/(?=[A-Z])/)
-                        .join('-')
-                        .toLowerCase()}.js`
-                    )
-                }
-            },
-            'react-icons'
-        ], [
-            'transform-imports', {
-                '@redhat-cloud-services/frontend-components': {
-                    transform: '@redhat-cloud-services/frontend-components/',
-                    preventFullImport: true,
-                    skipDefaultConversion: true
-                },
-                '@redhat-cloud-services/frontend-components-notifications': {
-                    transform: '@redhat-cloud-services/frontend-components-notifications',
-                    preventFullImport: true,
-                    skipDefaultConversion: true
-                }
-            }
-        ]
-
-    ]
+    '@babel/react',
+  ],
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-object-rest-spread',
+    'babel-plugin-lodash',
+    '@babel/plugin-transform-react-display-name',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      'transform-imports',
+      {
+        '@patternfly/react-icons': {
+          transform: (importName) =>
+            `@patternfly/react-icons/dist/esm/icons/${
+              IconMapper[importName] ||
+              importName
+                .split(/(?=[A-Z])/)
+                .join('-')
+                .toLowerCase()
+            }.js`,
+        },
+      },
+      'react-icons',
+    ],
+    [
+      'transform-imports',
+      {
+        '@redhat-cloud-services/frontend-components': {
+          transform: '@redhat-cloud-services/frontend-components/',
+          preventFullImport: true,
+          skipDefaultConversion: true,
+        },
+        '@redhat-cloud-services/frontend-components-notifications': {
+          transform: '@redhat-cloud-services/frontend-components-notifications',
+          preventFullImport: true,
+          skipDefaultConversion: true,
+        },
+      },
+    ],
+  ],
 };
