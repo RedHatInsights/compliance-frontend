c0256a0b5a9bfe210c9bcb03320c0e05dd86af72
eshamard <eshamard@redhat.com>
2023-01-25 21:17:53 +0100

    fix(PolicyTypeTable): RHICOMPL-3153 - Fix policy type search

--- src/SmartComponents/CreatePolicy/Components/PolicyTypeTable.js
+++ src/SmartComponents/CreatePolicy/Components/PolicyTypeTable.js
@@ -6,6 +6,7 @@ import { TableToolsTable } from 'Utilities/hooks/useTableTools';
 import { renderComponent } from 'Utilities/helpers';
 import { conditionalFilterType } from '@redhat-cloud-services/frontend-components/ConditionalFilter';
 import PolicyTypeDetailsRow from './PolicyTypeDetailsRow';
+import { emptyRows } from 'Utilities/hooks/useTableTools/Components/NoResultsTable';
 
 const NameCell = ({ name, disabled }) => {
   return (
@@ -21,61 +22,66 @@ NameCell.propTypes = {
   disabled: propTypes.boolean,
 };
 
-const PolicyTypeTable = ({ profiles, onChange, selectedProfile }) => (
-  <TableToolsTable
-    items={profiles.map((profile) => ({
-      ...profile,
-      rowProps: {
-        selected: profile.id === selectedProfile?.id,
-        disableSelection: profile.disabled,
-      },
-    }))}
-    filters={{
-      filterConfig: [
-        {
-          type: conditionalFilterType.text,
-          label: 'Policy Name',
-          filter: (policyTypes, value) =>
-            policyTypes.filter((policyType) =>
-              policyType?.name.toLowerCase().includes(value.toLowerCase())
-            ),
-        },
+const PolicyTypeTable = ({ profiles, onChange, selectedProfile }) => {
+  const columns = [
+    {
+      title: 'Policy name',
+      key: 'name',
+      transforms: [
+        info({
+          tooltip:
+            'In use policies have already been used and therefore can not be applied to another SCAP Policy under the selected OS.',
+        }),
       ],
-    }}
-    columns={[
-      {
-        title: 'Policy name',
-        key: 'name',
-        transforms: [
-          info({
-            tooltip:
-              'In use policies have already been used and therefore can not be applied to another SCAP Policy under the selected OS.',
-          }),
+      sortByProp: 'name',
+      renderFunc: renderComponent(NameCell),
+    },
+    {
+      title: 'Supported OS versions',
+      transforms: [fitContent],
+      sortByProp: 'supportedOsVersions',
+      renderFunc: (_data, _id, profile) =>
+        profile.supportedOsVersions.join(', '),
+    },
+  ];
+
+  return (
+    <TableToolsTable
+      items={profiles.map((profile) => ({
+        ...profile,
+        rowProps: {
+          selected: profile.id === selectedProfile?.id,
+          disableSelection: profile.disabled,
+        },
+      }))}
+      filters={{
+        filterConfig: [
+          {
+            type: conditionalFilterType.text,
+            label: 'Policy Name',
+            filter: (policyTypes, value) =>
+              policyTypes.filter((policyType) =>
+                policyType?.name.toLowerCase().includes(value.toLowerCase())
+              ),
+          },
         ],
-        sortByProp: 'name',
-        renderFunc: renderComponent(NameCell),
-      },
-      {
-        title: 'Supported OS versions',
-        transforms: [fitContent],
-        sortByProp: 'supportedOsVersions',
-        renderFunc: (_data, _id, profile) =>
-          profile.supportedOsVersions.join(', '),
-      },
-    ]}
-    options={{
-      detailsComponent: PolicyTypeDetailsRow,
-      onRadioSelect: (_event, _value, _rowIdx, { itemId }) =>
-        onChange && onChange(profiles.find(({ id }) => id === itemId)),
-      sortBy: {
-        index: 2,
-        direction: 'asc',
-        property: 'name',
-      },
-    }}
-    variant="compact"
-  />
-);
+      }}
+      columns={columns}
+      options={{
+        detailsComponent: PolicyTypeDetailsRow,
+        onRadioSelect: (_event, _value, _rowIdx, { itemId }) =>
+          onChange && onChange(profiles.find(({ id }) => id === itemId)),
+        sortBy: {
+          index: 2,
+          direction: 'asc',
+          property: 'name',
+        },
+        emptyRows: emptyRows('policy types', columns.length),
+      }}
+      variant="compact"
+    />
+  );
+};
 
 PolicyTypeTable.propTypes = {
   profiles: propTypes.array,
