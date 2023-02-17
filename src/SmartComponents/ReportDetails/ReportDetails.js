2b36c7c4f6ad643c6e5ef95e0688739e4f331811
Michael Johnson <johnsonm325@gmail.com>
2023-02-17 13:44:36 -0500

    fix: Fix reports systems count and bar chart (#1862)
    This PR replaces the bar chart on the reports table with the donut chart. It also fixes the counts which were previously incorrect.
    
    The "total number of systems" considered in the main reports view do not take into account neither the reported or unsupported systems in the X of Y systems.
    
    Though, the bar chart displayed on each of the report entries does take them into account (displaying the unsupported systems in yellow, and the never-reported systems in gray)
    
    Steps to Reproduce:
    
    have one unsupported system reporting
    have 2 more additional systems associated to the reported policy
    Check reports page
    
    Co-authored-by: Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>

--- src/SmartComponents/ReportDetails/ReportDetails.js
+++ src/SmartComponents/ReportDetails/ReportDetails.js
@@ -1,19 +1,14 @@
 /* eslint-disable react/display-name */
 import React from 'react';
-import black300 from '@patternfly/react-tokens/dist/esm/global_palette_black_300';
-import blue200 from '@patternfly/react-tokens/dist/esm/chart_color_blue_200';
-import blue300 from '@patternfly/react-tokens/dist/esm/chart_color_blue_300';
 import propTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
-import { ChartDonut, ChartThemeVariant } from '@patternfly/react-charts';
 import {
   Breadcrumb,
   BreadcrumbItem,
   Grid,
   GridItem,
-  Text,
 } from '@patternfly/react-core';
 import PageHeader, {
   PageHeaderTitle,
@@ -21,7 +16,6 @@ import PageHeader, {
 import Main from '@redhat-cloud-services/frontend-components/Main';
 import EmptyTable from '@redhat-cloud-services/frontend-components/EmptyTable';
 import Spinner from '@redhat-cloud-services/frontend-components/Spinner';
-import { fixedPercentage, pluralize } from 'Utilities/TextHelper';
 import {
   BackgroundLink,
   BreadcrumbLinkItem,
@@ -29,7 +23,6 @@ import {
   ReportDetailsDescription,
   StateViewWithError,
   StateViewPart,
-  UnsupportedSSGVersion,
   SubPageTitle,
   LinkButton,
 } from 'PresentationalComponents';
@@ -40,6 +33,7 @@ import '@/Charts.scss';
 import './ReportDetails.scss';
 import * as Columns from '../SystemsTable/Columns';
 import { default as ReportDetailsWithNotReportedSystems } from './ReportDetailsWithNotReportedSystems';
+import ReportChart from './Components/ReportChart';
 
 export const QUERY = gql`
   query Profile($policyId: String!) {
@@ -73,53 +67,14 @@ export const ReportDetails = ({ route }) => {
     variables: { policyId },
     fetchPolicy: 'no-cache',
   });
-  let donutValues = [];
-  let donutId = 'loading-donut';
-  let chartColorScale;
   let profile = {};
   let policyName;
-  let legendData;
-  let compliancePercentage;
   let pageTitle;
 
   if (!loading && data) {
     profile = data.profile;
     policyName = profile.policy.name;
     pageTitle = `Report: ${policyName}`;
-    const compliantHostCount = profile.compliantHostCount;
-    const testResultHostCount = profile.testResultHostCount;
-    donutId = profile.name.replace(/ /g, '');
-    donutValues = [
-      { x: 'Compliant', y: testResultHostCount ? compliantHostCount : '0' },
-      { x: 'Non-compliant', y: testResultHostCount - compliantHostCount },
-    ];
-    chartColorScale = (testResultHostCount === 0 && [black300.value]) || [
-      blue300.value,
-      blue200.value,
-    ];
-    legendData = [
-      {
-        name:
-          donutValues[0].y +
-          ' ' +
-          pluralize(donutValues[0].y, 'system') +
-          ' compliant',
-      },
-      {
-        name:
-          donutValues[1].y +
-          ' ' +
-          pluralize(donutValues[1].y, 'system') +
-          ' non-compliant',
-      },
-    ];
-    compliancePercentage = testResultHostCount
-      ? fixedPercentage(
-          Math.floor(
-            100 * (donutValues[0].y / (donutValues[0].y + donutValues[1].y))
-          )
-        )
-      : 0;
   }
 
   useTitleEntity(route, policyName);
@@ -183,38 +138,13 @@ export const ReportDetails = ({ route }) => {
             <GridItem sm={12} md={12} lg={12} xl={6}>
               <div className="chart-inline">
                 <div className="chart-container">
-                  <ChartDonut
-                    data={donutValues}
-                    identifier={donutId}
-                    title={compliancePercentage}
-                    subTitle="Compliant"
-                    themeVariant={ChartThemeVariant.light}
-                    colorScale={chartColorScale}
-                    style={{ fontSize: 20 }}
-                    constrainToVisibleArea={true}
-                    innerRadius={88}
-                    width={462}
-                    legendPosition="right"
-                    legendData={legendData}
-                    legendOrientation="vertical"
-                    padding={{
-                      bottom: 20,
-                      left: 0,
-                      right: 250,
-                      top: 20,
-                    }}
+                  <ReportChart
+                    profile={profile}
+                    hasLegend={true}
+                    chartClass="report-details-chart-container"
                   />
                 </div>
               </div>
-              {profile.unsupportedHostCount > 0 && (
-                <Text>
-                  <UnsupportedSSGVersion showHelpIcon>
-                    <strong className="ins-c-warning-text">
-                      {profile.unsupportedHostCount} systems not supported
-                    </strong>
-                  </UnsupportedSSGVersion>
-                </Text>
-              )}
             </GridItem>
             <GridItem sm={12} md={12} lg={12} xl={6}>
               <ReportDetailsDescription profile={profile} />
