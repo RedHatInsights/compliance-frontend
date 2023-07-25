6799c8d9dca19bd8ae3c27c97fea86bd15bf3df6
Sebastian Gräßl <mail@bastilian.me>
2023-07-25 10:38:24 +0200

    chore: Remove obsolete chrome app registration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import Routes from './Routes';
 import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
 import { RBACProvider } from '@redhat-cloud-services/frontend-components/RBACProvider';
@@ -10,21 +9,11 @@ import './App.scss';
 import { useSetFlagsFromUrl } from 'Utilities/hooks/useFeature';
 
 const App = (props) => {
-  const location = useLocation();
   const chrome = useChrome();
 
   useSetFlagsFromUrl();
   useEffect(() => {
     chrome.hideGlobalFilter();
-
-    const baseComponentUrl = location.pathname.split('/')[1] || 'reports';
-    const unregister = chrome.on('APP_NAVIGATION', (event) => {
-      if (event.domEvent) {
-        chrome.appNavClick({ id: baseComponentUrl });
-      }
-    });
-
-    return () => unregister();
   }, []);
 
   return (
