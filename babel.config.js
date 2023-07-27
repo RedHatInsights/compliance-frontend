5e032e5765ff73fe9f68a779d28e9343d7492c24
Sebastian Gräßl <mail@bastilian.me>
2023-07-28 01:46:17 +0200

    chore(deps): Clean up Babel environment

--- babel.config.js
+++ babel.config.js
@@ -18,19 +18,9 @@ module.exports = {
     '@babel/react',
   ],
   plugins: [
-    [
-      '@babel/plugin-proposal-decorators',
-      {
-        legacy: true,
-      },
-    ],
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-object-rest-spread',
     'babel-plugin-lodash',
-    '@babel/plugin-transform-react-display-name',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-optional-chaining',
     [
       'transform-imports',
       {
--- src/SmartComponents/InventoryDetails/InventoryDetails.js
+++ src/SmartComponents/InventoryDetails/InventoryDetails.js
@@ -3,41 +3,30 @@ import propTypes from 'prop-types';
 import Skeleton, {
   SkeletonSize,
 } from '@redhat-cloud-services/frontend-components/Skeleton';
-import { registryDecorator } from '@redhat-cloud-services/frontend-components-utilities/Registry';
+import { init } from 'Store';
+
 import {
   DetailWrapper,
   InventoryDetailHead,
 } from '@redhat-cloud-services/frontend-components/Inventory';
 
-@registryDecorator()
-class InventoryDetails extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <DetailWrapper
-        onLoad={({ mergeWithDetail }) =>
-          this.getRegistry().register({
-            ...mergeWithDetail(),
-          })
-        }
-        inventoryId={this.props.inventoryId}
-      >
-        <InventoryDetailHead fallback={<Skeleton size={SkeletonSize.md} />} />
-      </DetailWrapper>
-    );
-  }
-}
+const InventoryDetails = ({ inventoryId }) => {
+  return (
+    <DetailWrapper
+      onLoad={({ mergeWithDetail }) =>
+        init().register({
+          ...mergeWithDetail(),
+        })
+      }
+      inventoryId={inventoryId}
+    >
+      <InventoryDetailHead fallback={<Skeleton size={SkeletonSize.md} />} />
+    </DetailWrapper>
+  );
+};
 
 InventoryDetails.propTypes = {
-  entity: propTypes.object,
   inventoryId: propTypes.string,
 };
 
-const ConnectedInventoryDetails = (props) => {
-  return <InventoryDetails {...props} />;
-};
-
-export default ConnectedInventoryDetails;
+export default InventoryDetails;
