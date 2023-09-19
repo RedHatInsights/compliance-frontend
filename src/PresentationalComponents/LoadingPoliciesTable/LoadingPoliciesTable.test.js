4c00de92dd6364a9b4d75e3eb8dcf1d8f866fb8e
Sebastian Gräßl <mail@bastilian.me>
2023-09-19 23:08:22 +0200

    chore(LoadingPoliciesTable): Convert tests to testing-library

--- src/PresentationalComponents/LoadingPoliciesTable/LoadingPoliciesTable.test.js
+++ src/PresentationalComponents/LoadingPoliciesTable/LoadingPoliciesTable.test.js
@@ -1,9 +1,17 @@
+import { render } from '@testing-library/react';
+import { queryByText, queryByLabelText } from '@testing-library/dom';
+import columns from '../PoliciesTable/Columns';
+
 import LoadingPoliciesTable from './LoadingPoliciesTable';
 
 describe('LoadingPoliciesTable', () => {
   it('expect to render without error', () => {
-    const wrapper = shallow(<LoadingPoliciesTable />);
+    const { container } = render(<LoadingPoliciesTable />);
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(queryByLabelText(container, 'policies-table')).not.toBeNull();
+
+    columns.forEach(({ title }) => {
+      expect(queryByText(container, title)).not.toBeNull();
+    });
   });
 });
