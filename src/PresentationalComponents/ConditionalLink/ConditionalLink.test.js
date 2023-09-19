64caf777d94f933ba6f16e6e577067d094a70d37
Sebastian Gräßl <mail@bastilian.me>
2023-09-19 23:06:49 +0200

    chore(ConditionalLink): Convert tests to testing-library

--- src/PresentationalComponents/ConditionalLink/ConditionalLink.test.js
+++ src/PresentationalComponents/ConditionalLink/ConditionalLink.test.js
@@ -1,19 +1,33 @@
+import { render } from '@testing-library/react';
+import { queryByText } from '@testing-library/dom';
+
 import ConditionalLink from './ConditionalLink';
 
 describe('ConditionalLink', () => {
-  it('expect to render without error', () => {
-    const wrapper = shallow(<ConditionalLink href="https://redhat.com" />);
+  it('expect to not render a link with no href passed', () => {
+    const { container } = render(
+      <ConditionalLink>
+        <span>Test Child</span>
+      </ConditionalLink>
+    );
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(container.querySelector('a')).toBeNull();
   });
 
-  it('expect to render without error and children', () => {
-    const wrapper = shallow(
+  it('expect to render a link with a href passed', () => {
+    const { container } = render(<ConditionalLink href="https://redhat.com" />);
+
+    expect(container.querySelector('a')).not.toBeNull();
+  });
+
+  it('expect to render a link and children with a href passed', () => {
+    const { container } = render(
       <ConditionalLink href="https://redhat.com">
         <span>Test Child</span>
       </ConditionalLink>
     );
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(container.querySelector('a')).not.toBeNull();
+    expect(queryByText(container, 'Test Child')).not.toBeNull();
   });
 });
