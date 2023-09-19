89292a3540ebec62a05f6a844d46085a75c9f405
Sebastian Gräßl <mail@bastilian.me>
2023-09-19 23:08:44 +0200

    chore(NoResultsTable): Convert tests to testing-library

--- src/PresentationalComponents/NoResultsTable/NoResultsTable.test.js
+++ src/PresentationalComponents/NoResultsTable/NoResultsTable.test.js
@@ -1,15 +1,18 @@
+import { render } from '@testing-library/react';
+import { queryByText } from '@testing-library/dom';
+
 import NoResultsTable, { emptyRows } from './NoResultsTable';
 
 describe('NoResultsTable', () => {
   it('expect to render without error', () => {
-    const wrapper = shallow(<NoResultsTable />);
+    const { container } = render(<NoResultsTable />);
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(queryByText(container, 'No matching policies found')).not.toBeNull();
   });
 });
 
 describe('emptyRows', () => {
-  it('expect to render without error', () => {
-    expect(emptyRows).toMatchSnapshot();
+  it('expect to return table props for an empty table with NoResultsTable', () => {
+    expect(emptyRows[0].cells[0].title()).toEqual(<NoResultsTable />);
   });
 });
