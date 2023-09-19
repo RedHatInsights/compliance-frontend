d7530b67d9fc9feb5d085706f9edf123295c2715
Sebastian Gräßl <mail@bastilian.me>
2023-09-19 23:06:27 +0200

    chore(ComplianceThresholdHelperText): Convert tests to testing-library

--- src/PresentationalComponents/ComplianceThresholdHelperText/ComplianceThresholdHelperText.test.js
+++ src/PresentationalComponents/ComplianceThresholdHelperText/ComplianceThresholdHelperText.test.js
@@ -1,15 +1,30 @@
+import { render } from '@testing-library/react';
+import { queryByText } from '@testing-library/dom';
+
 import ComplianceThresholdHelperText from './ComplianceThresholdHelperText';
 
 describe('ComplianceThresholdHelperText', () => {
-  const defaultProps = {
-    threshold: 100,
-  };
-
-  it('expect to render without error', () => {
-    const wrapper = shallow(
-      <ComplianceThresholdHelperText {...defaultProps} />
+  it('expect to render nothing when threshold is valid', () => {
+    const { container } = render(
+      <ComplianceThresholdHelperText threshold="100" />
     );
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(queryByText(container, 'Threshold')).toBeNull();
+  });
+
+  it('expect to render a note when the threshold is not valid', () => {
+    const { container } = render(
+      <ComplianceThresholdHelperText threshold="1222" />
+    );
+
+    expect(queryByText(container, 'between 0 and 100')).toBeNull();
+  });
+
+  it('expect to render a note when the threshold has too many decimal places', () => {
+    const { container } = render(
+      <ComplianceThresholdHelperText threshold="10.222222" />
+    );
+
+    expect(queryByText(container, 'maximum of one decimal place')).toBeNull();
   });
 });
