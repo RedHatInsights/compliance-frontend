6a8dc77febcf76d4a155d9713c7fecb50ba5a8dd
Georgii Karataev <georgkarat@gmail.com>
2023-11-30 15:40:59 +0100

    chore(RHINENG-2189): Add IDs to CreatePolicyFooter

--- src/SmartComponents/CreatePolicy/CreatePolicyFooter.js
+++ src/SmartComponents/CreatePolicy/CreatePolicyFooter.js
@@ -9,38 +9,50 @@ import React from 'react';
 const CreatePolicyFooter = () => (
   <WizardFooter>
     <WizardContextConsumer>
-      {({ onNext, onBack, onClose, activeStep }) => (
-        <>
-          <Button
-            variant={ButtonVariant.primary}
-            type="submit"
-            onClick={onNext}
-            isDisabled={
-              !(activeStep && activeStep.enableNext !== undefined
-                ? activeStep.enableNext
-                : true)
-            }
-          >
-            {activeStep.nextButtonText || 'Next'}
-          </Button>
-          {!activeStep.hideBackButton && (
+      {({ onNext, onBack, onClose, activeStep }) => {
+        const stepNameEdited = activeStep.name
+          .toLowerCase()
+          .replaceAll(' ', '-');
+
+        return (
+          <>
             <Button
-              variant={ButtonVariant.secondary}
-              onClick={onBack}
-              isDisabled={activeStep.id === 1}
+              id={`${stepNameEdited}-next-button`}
+              variant={ButtonVariant.primary}
+              type="submit"
+              onClick={onNext}
+              isDisabled={
+                !(activeStep && activeStep.enableNext !== undefined
+                  ? activeStep.enableNext
+                  : true)
+              }
             >
-              {activeStep.backButtonText || 'Back'}
+              {activeStep.nextButtonText || 'Next'}
             </Button>
-          )}
-          {!activeStep.hideCancelButton && (
-            <div className="pf-c-wizard__footer-cancel">
-              <Button variant={ButtonVariant.link} onClick={onClose}>
-                {activeStep.cancelButtonText || 'Cancel'}
+            {!activeStep.hideBackButton && (
+              <Button
+                id={`${stepNameEdited}-back-button`}
+                variant={ButtonVariant.secondary}
+                onClick={onBack}
+                isDisabled={activeStep.id === 1}
+              >
+                {activeStep.backButtonText || 'Back'}
               </Button>
-            </div>
-          )}
-        </>
-      )}
+            )}
+            {!activeStep.hideCancelButton && (
+              <div className="pf-c-wizard__footer-cancel">
+                <Button
+                  id={`${stepNameEdited}-cancel-button`}
+                  variant={ButtonVariant.link}
+                  onClick={onClose}
+                >
+                  {activeStep.cancelButtonText || 'Cancel'}
+                </Button>
+              </div>
+            )}
+          </>
+        );
+      }}
     </WizardContextConsumer>
   </WizardFooter>
 );
