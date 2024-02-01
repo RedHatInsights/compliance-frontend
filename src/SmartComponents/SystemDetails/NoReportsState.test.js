f37f537192eac2c417c919abef87e8a7c9eb85d7
Sebastian Gräßl <mail@bastilian.me>
2024-02-01 21:14:39 +0100

    chore(SystemDetails): Convert tests to testing-library

--- src/SmartComponents/SystemDetails/NoReportsState.test.js
+++ src/SmartComponents/SystemDetails/NoReportsState.test.js
@@ -1,15 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import NoReportsState from './NoReportsState';
 
 describe('NoReportsState', () => {
   it('with a system having policies', () => {
-    const wrapper = shallow(<NoReportsState system={{ policies: [{}] }} />);
+    render(<NoReportsState system={{ policies: [{}] }} />);
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(
+      screen.getByText(
+        'This system is part of 1 policy, but has not returned any results.'
+      )
+    ).toBeInTheDocument();
   });
 
   it('with a system having multiple policies', () => {
-    const wrapper = shallow(<NoReportsState system={{ policies: [{}, {}] }} />);
+    render(<NoReportsState system={{ policies: [{}, {}] }} />);
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(
+      screen.getByText(
+        'This system is part of 2 policies, but has not returned any results.'
+      )
+    ).toBeInTheDocument();
   });
 });
