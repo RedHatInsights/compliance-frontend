b428809e2b9e1d3aa4175785dd36a138179971d8
Martin Michalik <xmicha82@vutbr.cz>
2024-04-04 17:23:20 +0200

    fix(ComplianceDetails): RHINENG-8348 fix perms in inventory

--- src/Modules/ComplianceDetails.js
+++ src/Modules/ComplianceDetails.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client';
 import { Provider } from 'react-redux';
 import { init } from 'Store';
 import Details from '../SmartComponents/SystemDetails/ComplianceDetail';
+import { RBACProvider } from '@redhat-cloud-services/frontend-components/RBACProvider';
 
 const ComplianceDetails = (props) => {
   const store = useRef(init().getStore());
@@ -18,11 +19,13 @@ const ComplianceDetails = (props) => {
   );
 
   return (
-    <ApolloProvider client={client.current}>
-      <Provider store={store.current}>
-        <Details {...props} />
-      </Provider>
-    </ApolloProvider>
+    <RBACProvider appName="compliance">
+      <ApolloProvider client={client.current}>
+        <Provider store={store.current}>
+          <Details {...props} />
+        </Provider>
+      </ApolloProvider>
+    </RBACProvider>
   );
 };
 
