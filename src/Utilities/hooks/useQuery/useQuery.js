5e3229c2156dae0d54c13ac71d78ee240d22bb92
AsToNlele <theastonofficial@gmail.com>
2024-07-30 17:22:16 +0200

    feat(useQuery): serializer option

--- src/Utilities/hooks/useQuery/useQuery.js
+++ src/Utilities/hooks/useQuery/useQuery.js
@@ -6,7 +6,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
  * @typedef {Object} Options
  * @property {Array} [params] - An array of parameters for the query function.
  * @property {boolean} [skip=false] - A boolean flag to skip the execution of the query function.
- *
+ * @property {Object} [serializer=undefined] - Settings to change the structure of the response
  *
  * @param {Function} fn - Function to execute
  * @param {Options} options - Includes options like params and skip
@@ -21,7 +21,10 @@ import { useCallback, useEffect, useRef, useState } from 'react';
  * @example
  * const query = useQuery(apiInstance.system, {params: ["id"]})
  */
-const useQuery = (fn, { params = [], skip = false } = {}) => {
+const useQuery = (
+  fn,
+  { params = [], skip = false, serializer = undefined } = {}
+) => {
   const [data, setData] = useState(undefined);
   const [error, setError] = useState(undefined);
   const [loading, setLoading] = useState(true);
@@ -32,20 +35,79 @@ const useQuery = (fn, { params = [], skip = false } = {}) => {
   fnRef.current = fn;
   paramsRef.current = params;
 
+  /**
+   * @param {Object} input
+   * @param {Object} settings
+   *
+   * @example
+   * const input = {
+   *  id: "123",
+   *  deeply: {
+   *    nested: "value"
+   *  },
+   *  ref_id: "ref123"
+   * }
+   * const settings = {
+   *    id: "id",
+   *    "deeply.nested": "even.deep.err",
+   *    "ref_id": "refId"
+   * }
+   * res = serialize(input, settings);
+   * // res = {
+   * //   id: "123",
+   * //   even: {
+   * //    deep: {
+   * //      err: {
+   * //        value
+   * //      }
+   * //    }
+   * //   },
+   * //   refId: "ref123"
+   * // }
+   */
+  const serialize = (input, settings) => {
+    const getValue = (obj, key) => {
+      let ref = obj;
+      if (key.includes('.')) {
+        key.split('.').forEach((element) => {
+          ref = ref[element];
+        });
+      } else {
+        ref = ref[key];
+      }
+      return ref;
+    };
+
+    let newObj = {};
+    for (const [key, value] of Object.entries(settings)) {
+      const finalValue = getValue(input, key);
+      if (value.includes('.')) {
+        const split = value.split('.');
+        let ref = newObj;
+        split.forEach((el, index) => {
+          if (!Object.prototype.hasOwnProperty.call(ref, el)) {
+            ref[el] = index + 1 !== split.length ? {} : finalValue;
+          }
+          ref = ref[el];
+        });
+      } else {
+        newObj[value] = input[key];
+      }
+    }
+    return newObj;
+  };
+
   const refetch = useCallback(async () => {
     setLoading(true);
     try {
-      const data = await fnRef.current(...paramsRef.current);
-      if (data?.data) {
-        setData(data.data);
-      } else {
-        setData(data);
-      }
+      const res = await fnRef.current(...paramsRef.current);
+      const data = serializer ? serialize(res.data, serializer) : res;
+      setData(data.data);
     } catch (e) {
       console.log(e);
       setError(e);
     } finally {
-      setLoading(false);
+      setLoading(undefined);
     }
   }, [fnRef, paramsRef]);
 
