041ef22a520fddb7e99ddb4f254ac939f30571b9
Sebastian Gräßl <mail@bastilian.me>
2024-07-31 22:01:20 +0200

    docs(AsyncTableTools): Add docs for useItems hook

--- src/Frameworks/AsyncTableTools/hooks/useAsyncTableTools/useItems.js
+++ src/Frameworks/AsyncTableTools/hooks/useAsyncTableTools/useItems.js
@@ -1,6 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 import { useSerialisedTableState, useRawTableState } from '../useTableState';
 
+/**
+ *  This hook handles either just returning a provided array of items
+ *  or calls a provided async (fetch) function to load an array of items
+ *
+ *  @param {Array | Function} itemsProp An array or (async) function that returns an array of items to render or an async function to call with the tableState and serialised table state
+ *
+ *  @returns {Array} An array of items from the itemsProp passed in or a return from it as a function
+ *
+ *  @category AsyncTableTools
+ *  @subcategory Hooks
+ *
+ */
 const useItems = (itemsProp) => {
   const mounted = useRef(true);
   const [items, setItems] = useState();
