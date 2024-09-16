b7f1341fc5f0eac9ec4e46c3ae9b2f8008ca2443
Adonis Puente <60629070+adonispuente@users.noreply.github.com>
2024-09-16 07:44:17 -0500

    feat(RHINENG-11722): Migrate Delete Report Modal to APIV2 (#2153)
    * feat(RHINENG-11722): Migrate Delete Report Modal to APIV2

--- src/SmartComponents/DeleteReport/DeleteReport.js
+++ src/SmartComponents/DeleteReport/DeleteReport.js
@@ -1,50 +1,35 @@
-import { Button, ModalVariant, TextContent } from '@patternfly/react-core';
-import propTypes from 'prop-types';
 import React from 'react';
+import {
+  Button,
+  ModalVariant,
+  TextContent,
+  Spinner,
+  Bullseye,
+} from '@patternfly/react-core';
+import propTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
-import useNavigate from '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate';
 import { useMutation } from '@apollo/client';
 import { DELETE_REPORT } from 'Mutations';
-import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/redux';
 import { ComplianceModal } from 'PresentationalComponents';
-import { dispatchAction } from 'Utilities/Dispatcher';
+import useNavigate from '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate';
+import useAPIV2FeatureFlag from '../../Utilities/hooks/useAPIV2FeatureFlag';
+import { apiInstance } from '../../Utilities/hooks/useQuery';
+import { handleDeleteReport } from '../CreatePolicy/helpers';
 
-const DeleteReport = () => {
-  const navigate = useNavigate();
-  const { report_id: id } = useParams();
-
-  const onClose = () => {
-    navigate(-1);
-  };
-
-  const onDelete = () => {
-    navigate('/reports');
-  };
-
-  const [deleteReport] = useMutation(DELETE_REPORT, {
-    onCompleted: () => {
-      dispatchAction(
-        addNotification({
-          variant: 'success',
-          title: 'Report deleted',
-          description:
-            'Systems associated with this policy will upload reports on the next check-in.',
-        })
-      );
-      onDelete();
-    },
-    onError: (error) => {
-      dispatchAction(
-        addNotification({
-          variant: 'danger',
-          title: 'Error removing report',
-          description: error.message,
-        })
-      );
-      onClose();
-    },
-  });
-
+/**
+ * DeleteReportBase Component
+ *
+ * Renders the base modal for deleting a report. It provides buttons for confirming
+ * or canceling the deletion.
+ *
+ * @param {object} props - The properties passed to the component.
+ * @param {string} props.id - The ID of the report to be deleted.
+ * @param {Function} props.deleteReport - Function to call when the delete button is clicked.
+ * @param {Function} props.onClose - Function to call when the cancel button or modal close is triggered.
+ *
+ * @returns {React.Element} The rendered DeleteReportBase component.
+ */
+const DeleteReportBase = ({ id, deleteReport, onClose }) => {
   return (
     <ComplianceModal
       isOpen
@@ -59,15 +44,7 @@ const DeleteReport = () => {
           ouiaId="DeleteReportButton"
           aria-label="delete"
           variant="danger"
-          onClick={() =>
-            deleteReport({
-              variables: {
-                input: {
-                  profileId: id,
-                },
-              },
-            })
-          }
+          onClick={() => deleteReport(id)}
         >
           Delete report
         </Button>,
@@ -88,16 +65,124 @@ const DeleteReport = () => {
   );
 };
 
-DeleteReport.propTypes = {
+DeleteReportBase.propTypes = {
+  id: propTypes.string.isRequired,
   onClose: propTypes.func,
-  isModalOpen: propTypes.bool,
+  deleteReport: propTypes.func,
+};
+
+/**
+ * DeleteReportRest Component
+ *
+ * Handles report deletion using a REST API.
+ *
+ * @param {object} props - The properties passed to the component.
+ * @param {string} props.reportId - The ID of the report to be deleted.
+ * @param {Function} props.onClose - Function to call when the modal is closed.
+ * @param {Function} props.onDelete - Function to call when the report is successfully deleted.
+ *
+ * @returns {React.Element} The rendered DeleteReportRest component.
+ */
+const DeleteReportRest = ({ reportId, onClose, onDelete }) => {
+  const deleteReport = () =>
+    handleDeleteReport(
+      () => apiInstance.deleteReport(reportId),
+      onDelete,
+      onClose
+    );
+
+  return (
+    <DeleteReportBase
+      id={reportId}
+      deleteReport={deleteReport}
+      onClose={onClose}
+    />
+  );
+};
+
+DeleteReportRest.propTypes = {
+  reportId: propTypes.string.isRequired,
+  onClose: propTypes.func,
   onDelete: propTypes.func,
-  policyId: propTypes.string,
 };
 
-DeleteReport.defaultProps = {
-  onDelete: () => {},
-  onClose: () => {},
+/**
+ * DeleteReportGraphQL Component
+ *
+ * Handles report deletion using a GraphQL.
+ *
+ * @param {object} props - The properties passed to the component.
+ * @param {string} props.reportId - The ID of the report to be deleted.
+ * @param {Function} props.onClose - Function to call when the modal is closed.
+ * @param {Function} props.onDelete - Function to call when the report is successfully deleted.
+ *
+ * @returns {React.Element} The rendered DeleteReportGraphQL component.
+ */
+const DeleteReportGraphQL = ({ reportId, onClose, onDelete }) => {
+  const [callDeleteReport] = useMutation(DELETE_REPORT, {
+    onCompleted: () =>
+      handleDeleteReport(() => Promise.resolve(), onDelete, onClose),
+    onError: (error) =>
+      handleDeleteReport(() => Promise.reject(error), onDelete, onClose),
+  });
+
+  const deleteReport = () => {
+    callDeleteReport({ variables: { input: { profileId: reportId } } });
+  };
+
+  return (
+    <DeleteReportBase
+      id={reportId}
+      deleteReport={deleteReport}
+      onClose={onClose}
+    />
+  );
 };
 
-export default DeleteReport;
+DeleteReportGraphQL.propTypes = {
+  reportId: propTypes.string.isRequired,
+  onClose: propTypes.func,
+  onDelete: propTypes.func,
+};
+
+/**
+ * DeleteReportWrapper Component
+ *
+ * This component decides to show a loading spinner or the component once apiV2 is defined
+ *
+ * @returns {React.Element} The rendered DeleteReportWrapper component.
+ */
+const DeleteReportWrapper = () => {
+  const { report_id: reportId } = useParams();
+  const apiV2Enabled = useAPIV2FeatureFlag();
+  const navigate = useNavigate();
+  const onClose = () => {
+    navigate(-1);
+  };
+
+  const onDelete = () => {
+    navigate('/reports');
+  };
+
+  const DeleteReportComponent = apiV2Enabled
+    ? DeleteReportRest
+    : DeleteReportGraphQL;
+
+  return (
+    <>
+      {apiV2Enabled === undefined ? (
+        <Bullseye>
+          <Spinner />
+        </Bullseye>
+      ) : (
+        <DeleteReportComponent
+          reportId={reportId}
+          onClose={onClose}
+          onDelete={onDelete}
+        />
+      )}
+    </>
+  );
+};
+
+export default DeleteReportWrapper;
--- src/SmartComponents/DeleteReport/DeleteReport.test.js
+++ src/SmartComponents/DeleteReport/DeleteReport.test.js
@@ -1,38 +1,110 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import TestWrapper from '@/Utilities/TestWrapper';
-
+import { useParams } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import { dispatchAction } from 'Utilities/Dispatcher';
+import useNavigate from '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate';
+import useAPIV2FeatureFlag from '../../Utilities/hooks/useAPIV2FeatureFlag';
+import { apiInstance } from '../../Utilities/hooks/useQuery';
+import DeleteReportWrapper from './DeleteReport';
 
-jest.mock('@apollo/client');
-jest.mock('Utilities/Dispatcher');
+jest.mock('react-router-dom', () => ({
+  useParams: jest.fn(),
+}));
 
-import DeleteReport from './DeleteReport.js';
+jest.mock('@apollo/client', () => {
+  const actualApolloClient = jest.requireActual('@apollo/client');
+  return {
+    ...actualApolloClient,
+    useMutation: jest.fn(),
+    gql: jest.fn().mockImplementation((query) => query),
+  };
+});
 
-describe('DeleteReport', () => {
+jest.mock(
+  '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate',
+  () => ({
+    __esModule: true,
+    default: jest.fn(),
+  })
+);
+
+jest.mock('../../Utilities/hooks/useAPIV2FeatureFlag');
+jest.mock('../../Utilities/hooks/useQuery');
+jest.mock(
+  '@redhat-cloud-services/frontend-components-notifications/redux',
+  () => ({
+    addNotification: jest.fn(),
+  })
+);
+
+jest.mock('Utilities/Dispatcher', () => ({
+  dispatchAction: jest.fn(),
+}));
+
+describe('DeleteReportWrapper', () => {
+  const navigateMocked = jest.fn();
+  useNavigate.mockImplementation(() => navigateMocked);
+  const reportId = '12345';
+
   beforeEach(() => {
-    useMutation.mockImplementation((_query, options) => {
-      return [
-        function () {
-          options.onCompleted();
-        },
-      ];
+    jest.clearAllMocks();
+    useParams.mockReturnValue({ report_id: reportId });
+  });
+
+  test('renders spinner when apiV2Enabled is undefined', () => {
+    useAPIV2FeatureFlag.mockReturnValue(undefined);
+
+    render(<DeleteReportWrapper />);
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  test('renders DeleteReportRest when apiV2Enabled is true', async () => {
+    useAPIV2FeatureFlag.mockReturnValue(true);
+    apiInstance.deleteReport.mockResolvedValue({});
+
+    render(<DeleteReportWrapper />);
+
+    fireEvent.click(screen.getByText('Delete report'));
+
+    expect(apiInstance.deleteReport).toHaveBeenCalledWith(reportId);
+
+    await waitFor(() => {
+      expect(navigateMocked).toHaveBeenCalledWith('/reports');
     });
-    dispatchAction.mockImplementation(() => {});
   });
 
-  it('expect to render an open modal without error', () => {
-    render(
-      <TestWrapper>
-        <DeleteReport />
-      </TestWrapper>
-    );
+  test('renders DeleteReportGraphQL when apiV2Enabled is false', async () => {
+    const mockMutation = jest.fn(() => Promise.resolve());
+    useAPIV2FeatureFlag.mockReturnValue(false);
+    useMutation.mockReturnValue([
+      mockMutation,
+      { loading: false, error: null },
+    ]);
 
-    expect(
-      screen.getByText('Deleting a report is permanent and cannot be undone.')
-    ).toBeInTheDocument();
+    render(<DeleteReportWrapper />);
+
+    fireEvent.click(screen.getByText('Delete report'));
+
+    await waitFor(() => {
+      expect(mockMutation).toHaveBeenCalledWith({
+        variables: { input: { profileId: reportId } },
+      });
+    });
   });
 
-  // TODO this should also test the deleting as well
+  test('handles error during REST deletion', async () => {
+    useAPIV2FeatureFlag.mockReturnValue(true);
+    const error = new Error('Deletion failed');
+    apiInstance.deleteReport.mockRejectedValue(error);
+
+    render(<DeleteReportWrapper />);
+
+    fireEvent.click(screen.getByText('Delete report'));
+
+    await waitFor(() => {
+      expect(navigateMocked).toHaveBeenCalledWith(-1);
+    });
+  });
 });
