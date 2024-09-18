ffa5c2f22c9ec1e58a0e383298a0f951be008b21
Egor Shamardin <33912805+LightOfHeaven1994@users.noreply.github.com>
2024-09-18 12:20:28 +0200

    fix(ReportCharts): fix chart data & chart legend (#2156)

--- src/SmartComponents/ReportDetails/Components/ReportChart.test.js
+++ src/SmartComponents/ReportDetails/Components/ReportChart.test.js
@@ -1,7 +1,13 @@
 import { render } from '@testing-library/react';
 import ReportChart from './ReportChart';
+import useAPIV2FeatureFlag from '../../../Utilities/hooks/useAPIV2FeatureFlag';
+
+jest.mock('../../../Utilities/hooks/useAPIV2FeatureFlag');
 
 describe('ReportChart', () => {
+  beforeEach(() => {
+    useAPIV2FeatureFlag.mockImplementation(() => false);
+  });
   const defaultProps = {
     profile: {
       name: 'TEST Profile name',
@@ -66,6 +72,44 @@ describe('ReportChart', () => {
         testResultHostCount: 5,
         unsupportedHostCount: 0,
         totalHostCount: 10,
+      },
+    };
+    const component = <ReportChart {...props} />;
+    const { container } = render(component);
+
+    expect(container).toMatchSnapshot();
+  });
+});
+
+describe('ReportChartAPIv2', () => {
+  beforeEach(() => {
+    useAPIV2FeatureFlag.mockImplementation(() => true);
+  });
+  const defaultProps = {
+    profile: {
+      name: 'TEST Profile name',
+      compliantHostCount: 4,
+      testResultHostCount: 7,
+      unsupportedHostCount: 2,
+      totalHostCount: 10,
+    },
+  };
+
+  it('expect to render with 2 unsupported & 3 never reported hosts', () => {
+    const component = <ReportChart {...defaultProps} />;
+    const { container } = render(component);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('expect to render with 0 unsupported & 0 never reported hosts', () => {
+    const props = {
+      profile: {
+        ...defaultProps.profile,
+        compliantHostCount: 4,
+        testResultHostCount: 5,
+        unsupportedHostCount: 0,
+        totalHostCount: 5,
       },
     };
     const component = <ReportChart {...props} />;
