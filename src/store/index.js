f827c0a9104b9206bcae4903d76807988187b5c8
Georgii Karataev <georgkarat@gmail.com>
2024-09-30 13:32:13 +0200

    chore: Support Redux DevTools in the dev environment

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,22 @@ import { getRegistry } from '@redhat-cloud-services/frontend-components-utilitie
 import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import { reducer as form } from 'redux-form';
+import { compose } from 'redux';
 
 let registry;
 
 export function init(...middleware) {
-  registry = getRegistry({}, [
-    promiseMiddleware,
-    ...middleware.filter((item) => typeof item !== 'undefined'),
-  ]);
+  registry = getRegistry(
+    {},
+    [
+      promiseMiddleware,
+      ...middleware.filter((item) => typeof item !== 'undefined'),
+    ],
+    (process.env.NODE_ENV !== 'production' &&
+      typeof window !== 'undefined' &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+      compose
+  );
 
   registry.register({ form, notifications: notificationsReducer });
   return registry;
