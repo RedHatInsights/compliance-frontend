0d75187b6e4431015cf73dce4e3c977c416afb4b
Sebastian Gräßl <mail@bastilian.me>
2024-10-07 14:22:14 +0200

    feat(AsyncTable): Introduce "Table view" with tree table

---
+++ src/Frameworks/AsyncTableTools/hooks/useTableView/hooks/useViewState.js
@@ -0,0 +1,62 @@
+import { useCallback, useRef } from 'react';
+import { TABLE_STATE_NAMESPACE as FILTERS_TABLE_STATE_NAMESPACE } from '../../useFilterConfig/constants';
+import useTableState from '../../useTableState';
+import { DEFAULT_TABLE_VIEW, TABLE_STATE_NAMESPACE } from '../constants';
+
+const useViewState = (options) => {
+  const { defaultTableView = DEFAULT_TABLE_VIEW } = options;
+  const internalTableView = useRef(defaultTableView);
+  // TODO Possible turn this into a table feature
+  const filtersObserver = useCallback(
+    (currentTableViewState, previousFilters, nextFilters) => {
+      const newView =
+        nextFilters?.name?.[0] !== previousFilters?.name?.[0]
+          ? 'rows'
+          : internalTableView.current;
+
+      internalTableView.current = newView;
+      return newView;
+    },
+    []
+  );
+  const itemObserver = useCallback(
+    (currentTableViewState, _previousItems, items) => {
+      console.log('io', currentTableViewState, _previousItems, items);
+      let newView;
+      if (typeof items === 'undefined') {
+        newView = 'loading';
+      } else if (items.length === 0 && internalTableView.current !== 'tree') {
+        newView = 'empty';
+      } else if (items.length > 0) {
+        newView = internalTableView.current;
+      }
+
+      return newView;
+    },
+    []
+  );
+  const [tableView, setTableViewState] = useTableState(
+    TABLE_STATE_NAMESPACE,
+    defaultTableView,
+    {
+      observers: {
+        [FILTERS_TABLE_STATE_NAMESPACE]: filtersObserver,
+        items: itemObserver,
+      },
+    }
+  );
+  const setTableView = useCallback(
+    (view) => {
+      internalTableView.current = view;
+      setTableViewState(view);
+    },
+    [setTableViewState]
+  );
+
+  return {
+    setTableView,
+    tableView,
+  };
+};
+
+export default useViewState;
