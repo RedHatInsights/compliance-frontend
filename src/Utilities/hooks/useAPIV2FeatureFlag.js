09943bbe5d27d3e7d84c2ee8ad20ac471c6d5aa2
Sebastian Gräßl <bastilian@users.noreply.github.com>
2024-10-15 12:38:47 +0200

    feat(Utilities): Add localStorage feature flag for APIv2 (#2198)

--- src/Utilities/hooks/useAPIV2FeatureFlag.js
+++ src/Utilities/hooks/useAPIV2FeatureFlag.js
@@ -1,5 +1,11 @@
 import useFeatureFlag from './useFeatureFlag';
 
-const useAPIV2FeatureFlag = () => useFeatureFlag('compliance-api-v2');
+const useAPIV2FeatureFlag = () => {
+  const unleashFlag = useFeatureFlag('compliance-api-v2');
+  const localFlag =
+    localStorage.getItem('insights:compliance:apiv2') === 'true';
+
+  return localFlag || unleashFlag;
+};
 
 export default useAPIV2FeatureFlag;
