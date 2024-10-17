08248006cbd3049c1dbf68f0e19a81081ad4dd5b
Georgii Karataev <georgkarat@gmail.com>
2024-10-17 15:07:53 +0200

    fix(useSystemBulkSelect): Reset items on filters change
    This adds a reset callback to useLoadedItems hook. The function is called by useSystemBulkSelect to clear the cache if active filters have changed for the correct "select all" behaviour.

--- src/SmartComponents/SystemsTable/hooks/useLoadedItems.js
+++ src/SmartComponents/SystemsTable/hooks/useLoadedItems.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 import unionBy from '../../../Utilities/unionBy';
+import debounce from '@redhat-cloud-services/frontend-components-utilities/debounce';
 
 const useLoadedItems = (currentPageItems, total, key = 'id') => {
   const [loadedItems, setLoadedItems] = useState([]);
@@ -12,6 +13,15 @@ const useLoadedItems = (currentPageItems, total, key = 'id') => {
     [key]
   );
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const resetLoadedItems = useCallback(
+    debounce(() => {
+      setLoadedItems([]);
+      setAllLoaded(false);
+    }, 200),
+    []
+  );
+
   useEffect(() => {
     setAllLoaded(total <= loadedItems.length);
   }, [total, loadedItems]);
@@ -20,7 +30,7 @@ const useLoadedItems = (currentPageItems, total, key = 'id') => {
     addToLoadedItems(currentPageItems);
   }, [currentPageItems, addToLoadedItems]);
 
-  return { loadedItems, addToLoadedItems, allLoaded };
+  return { loadedItems, addToLoadedItems, resetLoadedItems, allLoaded };
 };
 
 export default useLoadedItems;
