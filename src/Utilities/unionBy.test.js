6f3f33777dc1e0a49042f5439614a80ff269774a
Georgii Karataev <georgkarat@gmail.com>
2024-10-18 16:06:39 +0200

    test: Add two new test for unionBy

--- src/Utilities/unionBy.test.js
+++ src/Utilities/unionBy.test.js
@@ -22,4 +22,18 @@ describe('unionBy', () => {
   it('union of two empty arrays', () => {
     expect(unionBy([], [], 'id')).toStrictEqual([]);
   });
+
+  it('union of two arrays with the same item', () => {
+    expect(unionBy([{ id: 1 }, { id: 2 }], [{ id: 2 }], 'id')).toStrictEqual([
+      { id: 1 },
+      { id: 2 },
+    ]);
+  });
+
+  it('union of just one array', () => {
+    expect(unionBy([{ id: 1 }, { id: 2 }], 'id')).toStrictEqual([
+      { id: 1 },
+      { id: 2 },
+    ]);
+  });
 });
