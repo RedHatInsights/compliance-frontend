dcdfb4818c632a55e142f90449ecf669dbb4d201
Adonis Puente <adonis.puente@gmail.com>
2024-10-18 11:37:56 -0500

    feat(sentry): add sentry to compliance

--- fec.config.js
+++ fec.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 const alias = require('./config/aliases');
 const packageJson = require('./package.json');
 const { localRoutesFor } = require('./config/helpers');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 const bundle = 'insights';
 const appName = packageJson[bundle].appname;
@@ -17,6 +18,25 @@ module.exports = {
   appName,
   appUrl: `/${bundle}/${appName}`,
   useProxy: process.env.PROXY === 'true',
+  devtool: 'hidden-source-map',
+  plugins: [
+    // Put the Sentry Webpack plugin after all other plugins
+    ...(process.env.SENTRY_AUTH_TOKEN
+      ? [
+          sentryWebpackPlugin({
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            org: 'red-hat-it',
+            project: 'compliance-rhel',
+            _experiments: {
+              moduleMetadata: ({ release }) => ({
+                dsn: `https://6410c806f0ac7b638105bb4e15eb3399@o490301.ingest.us.sentry.io/4508083145408512`,
+                release,
+              }),
+            },
+          }),
+        ]
+      : []),
+  ],
   moduleFederation: {
     shared: [
       {
