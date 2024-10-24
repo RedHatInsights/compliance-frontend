f3cbad9c06dab458b1985f2c5d5c83b351dc71ab
Sebastian Gräßl <mail@bastilian.me>
2024-10-24 12:48:21 +0200

    fix(Tailorings): Skip update request by default

--- src/Utilities/hooks/api/useUpdateTailoring.js
+++ src/Utilities/hooks/api/useUpdateTailoring.js
@@ -3,6 +3,7 @@ import useQuery, { apiInstance } from '../useQuery';
 const useUpdateTailoring = (policyId, tailoringId, tailoring, options) =>
   useQuery(apiInstance.updateTailoring, {
     params: [policyId, tailoringId, tailoring],
+    skip: true,
     ...options,
   });
 
