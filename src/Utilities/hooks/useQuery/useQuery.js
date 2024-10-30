57dd697dd4b59dc3162892d8ecf949f5daee2e6c
Sebastian Gräßl <mail@bastilian.me>
2024-10-30 11:24:53 +0100

    chore(useQuery): Update docs and explain fetch functions

--- src/Utilities/hooks/useQuery/useQuery.js
+++ src/Utilities/hooks/useQuery/useQuery.js
@@ -8,7 +8,8 @@ import debounce from '@redhat-cloud-services/frontend-components-utilities/debou
  *  @property {any}      [data]    Data loaded from the response of the request
  *  @property {Error}    [error]   An error if the request failed
  *  @property {boolean}  [loading] Boolean of wether or not data is being requested
- *  @property {Function} [refetch] A callback function to re-trigger a request
+ *  @property {Function} [refetch] A function to re-trigger a request with params coming from the options
+ *  @property {Function} [fetch]   A function to make a request on demand. It takes to arguments, a params object or array, and a boolean to control wether or not to set the "data" state or return the results directly. In case the params argument is an array any params from the options will be ignored!
  *
  */
 
@@ -20,7 +21,7 @@ import debounce from '@redhat-cloud-services/frontend-components-utilities/debou
  *  @param   {Array | object} [options.params] Parameters passed to the request to make. If an array is passed it will be spread as arguments!
  *  @param   {boolean}        [options.skip]   Wether or not to skip the request
  *
- *  @returns {useQueryReturn}
+ *  @returns {useQueryReturn}                  An object containing a data, loading and error state, as well as a fetch and refetch function.
  *
  * @example
  * // Query is skipped if conditions are met
