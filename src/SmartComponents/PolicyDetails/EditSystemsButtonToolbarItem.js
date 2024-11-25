c5e2d42d412f824045e5ec2159cc9b421848d1ac
Georgii Karataev <georgkarat@gmail.com>
2024-11-25 12:35:16 +0100

    fix: Temporarily hide the Edit policy feature/buttons (#2290)

--- src/SmartComponents/PolicyDetails/EditSystemsButtonToolbarItem.js
+++ src/SmartComponents/PolicyDetails/EditSystemsButtonToolbarItem.js
@@ -4,28 +4,36 @@ import {
   LinkWithPermission as Link,
   LinkButton,
 } from 'PresentationalComponents';
-import { ToolbarItem } from '@patternfly/react-core';
+import { Button, ToolbarItem, Tooltip } from '@patternfly/react-core';
 import useAnchor from 'Utilities/hooks/useAnchor';
+import useAPIV2FeatureFlag from '../../Utilities/hooks/useAPIV2FeatureFlag';
 
 const EditSystemsButtonToolbarItem = ({ policy }) => {
   const hash = useAnchor();
+  const apiV2Enabled = useAPIV2FeatureFlag();
 
   return (
     <ToolbarItem>
-      <Link
-        to={{
-          pathname: `/scappolicies/${policy.id}/edit`,
-          hash,
-        }}
-        state={{ returnTo: { pathname: `/scappolicies/${policy.id}`, hash } }}
-        Component={LinkButton}
-        componentProps={{
-          variant: 'primary',
-          ouiaId: 'EditSystemsButton',
-        }}
-      >
-        Edit systems
-      </Link>
+      {apiV2Enabled ? (
+        <Tooltip content="Switch to the stable version to access this functionality">
+          <Button isAriaDisabled>Edit systems</Button>
+        </Tooltip>
+      ) : (
+        <Link
+          to={{
+            pathname: `/scappolicies/${policy.id}/edit`,
+            hash,
+          }}
+          state={{ returnTo: { pathname: `/scappolicies/${policy.id}`, hash } }}
+          Component={LinkButton}
+          componentProps={{
+            variant: 'primary',
+            ouiaId: 'EditSystemsButton',
+          }}
+        >
+          Edit systems
+        </Link>
+      )}
     </ToolbarItem>
   );
 };
