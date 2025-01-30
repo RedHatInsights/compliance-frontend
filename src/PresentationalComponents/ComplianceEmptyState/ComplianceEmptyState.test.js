18b59755077a026dd1699ee4ba3b646cda47d459
LightOfHeaven1994 <eshamard@redhat.com>
2025-01-30 10:18:58 +0100

    chore(RHINENG-15653): Cleanup ComplianceEmptyState

--- src/PresentationalComponents/ComplianceEmptyState/ComplianceEmptyState.test.js
+++ src/PresentationalComponents/ComplianceEmptyState/ComplianceEmptyState.test.js
@@ -2,73 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import TestWrapper from '@/Utilities/TestWrapper';
-import { useQuery } from '@apollo/client';
-import useAPIV2FeatureFlag from '@/Utilities/hooks/useAPIV2FeatureFlag.js';
 import usePolicies from 'Utilities/hooks/api/usePolicies';
 
-jest.mock('@apollo/client');
-jest.mock('../../Utilities/hooks/useAPIV2FeatureFlag');
 jest.mock('Utilities/hooks/api/usePolicies', () => jest.fn());
 
 import ComplianceEmptyState from './ComplianceEmptyState';
 
 describe('ComplianceEmptyState', () => {
-  beforeEach(() => {
-    useAPIV2FeatureFlag.mockImplementation(() => false);
-  });
-  it('expect to render without error if no policies exist', () => {
-    useQuery.mockImplementation(() => ({
-      data: { profiles: { totalCount: 0 } },
-      error: undefined,
-      loading: undefined,
-    }));
-    render(
-      <TestWrapper>
-        <ComplianceEmptyState client={{}} />
-      </TestWrapper>
-    );
-
-    expect(screen.getByText('No policies')).toBeInTheDocument();
-  });
-
-  it('expect to render different message if one policy exists', () => {
-    useQuery.mockImplementation(() => ({
-      data: { profiles: { totalCount: 1 } },
-      error: undefined,
-      loading: undefined,
-    }));
-    render(
-      <TestWrapper>
-        <ComplianceEmptyState client={{}} />
-      </TestWrapper>
-    );
-
-    expect(screen.getByText('1 policy')).toBeInTheDocument();
-    expect(
-      screen.getByText('has been created but has no reports.')
-    ).toBeInTheDocument();
-  });
-
-  it('expect to render different message if many policies exist', () => {
-    useQuery.mockImplementation(() => ({
-      data: { profiles: { totalCount: 2 } },
-      error: undefined,
-      loading: undefined,
-    }));
-    render(
-      <TestWrapper>
-        <ComplianceEmptyState client={{}} />
-      </TestWrapper>
-    );
-
-    expect(screen.getByText('2 policies')).toBeInTheDocument();
-  });
-});
-
-describe('ComplianceEmptyState - REST', () => {
-  beforeEach(() => {
-    useAPIV2FeatureFlag.mockImplementation(() => true);
-  });
   it('expect to render without error if no policies exist', () => {
     usePolicies.mockImplementation(() => ({
       data: { meta: { total: 0 } },
@@ -77,7 +17,7 @@ describe('ComplianceEmptyState - REST', () => {
     }));
     render(
       <TestWrapper>
-        <ComplianceEmptyState client={{}} />
+        <ComplianceEmptyState />
       </TestWrapper>
     );
 
@@ -95,7 +35,7 @@ describe('ComplianceEmptyState - REST', () => {
     }));
     render(
       <TestWrapper>
-        <ComplianceEmptyState client={{}} />
+        <ComplianceEmptyState />
       </TestWrapper>
     );
 
@@ -114,7 +54,7 @@ describe('ComplianceEmptyState - REST', () => {
 
     render(
       <TestWrapper>
-        <ComplianceEmptyState client={{}} />
+        <ComplianceEmptyState />
       </TestWrapper>
     );
 
