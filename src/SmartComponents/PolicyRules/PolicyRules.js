4348f6cd25ab4c209b461fa038b78571a9544970
LightOfHeaven1994 <eshamard@redhat.com>
2025-02-25 15:44:40 +0100

    chore(PolicyRules): Cleanup Policy default rules component

--- src/SmartComponents/PolicyRules/PolicyRules.js
+++ src/SmartComponents/PolicyRules/PolicyRules.js
@@ -1,77 +1,16 @@
 import React, { useMemo } from 'react';
-import { useQuery, gql } from '@apollo/client';
 import { useParams } from 'react-router-dom';
-import propTypes from 'prop-types';
 import * as Columns from '@/PresentationalComponents/RulesTable/Columns';
-import RulesTable from '@/PresentationalComponents/RulesTable/RulesTable';
-import RulesTableRest from '@/PresentationalComponents/RulesTable/RulesTableRest';
+import RulesTable from '@/PresentationalComponents/RulesTable/RulesTableRest';
 import PolicyRulesHeader from './PolicyRulesHeader';
-import GatedComponents from 'PresentationalComponents/GatedComponents';
 import { usePolicyRulesList } from '@/Utilities/hooks/api/usePolicyRulesList';
 import { useFullTableState } from '@/Frameworks/AsyncTableTools/hooks/useTableState';
 import { policyRulesSkips } from './helpers';
 import TableStateProvider from '@/Frameworks/AsyncTableTools/components/TableStateProvider';
-import PageHeader from '@redhat-cloud-services/frontend-components/PageHeader';
-import { Spinner } from '@patternfly/react-core';
 import useSecurityGuide from 'Utilities/hooks/api/useSecurityGuide';
 import useProfile from 'Utilities/hooks/api/useProfile';
 
-const PROFILES_QUERY = gql`
-  query PR_Profile($policyId: String!) {
-    profile(id: $policyId) {
-      id
-      name
-      refId
-      osMinorVersion
-      osMajorVersion
-      benchmark {
-        version
-        ruleTree
-      }
-      rules {
-        id
-        title
-        severity
-        rationale
-        refId
-        description
-        remediationAvailable
-        identifier
-      }
-    }
-  }
-`;
-
-const PolicyRulesGraphQL = () => {
-  const { policy_id: policyId } = useParams();
-  const { data, loading } = useQuery(PROFILES_QUERY, {
-    variables: {
-      policyId: policyId,
-    },
-  });
-
-  return loading ? (
-    <PageHeader>
-      <Spinner />
-    </PageHeader>
-  ) : (
-    <PolicyRulesBase
-      name={data?.profile?.name}
-      benchmarkVersion={data?.profile?.benchmark?.version}
-      osMajorVersion={data?.profile?.osMajorVersion}
-    >
-      <RulesTable
-        remediationsEnabled={false}
-        columns={[Columns.Name, Columns.Severity, Columns.Remediation]}
-        loading={loading}
-        profileRules={[{ profile: data?.profile, rules: data?.profile?.rules }]}
-        options={{ pagination: false, manageColumns: false }}
-      />
-    </PolicyRulesBase>
-  );
-};
-
-const PolicyRulesRest = () => {
+const PolicyDefaultRules = () => {
   const tableState = useFullTableState();
   const { policy_id: profileId, security_guide_id: securityGuideId } =
     useParams();
@@ -119,59 +58,32 @@ const PolicyRulesRest = () => {
   // const rulesExporter = useRulesExporter(fetchRules);
 
   return (
-    <PolicyRulesBase
-      osMajorVersion={securityGuideData?.data?.os_major_version}
-      benchmarkVersion={securityGuideData?.data?.version}
-      headerName={profileData?.data?.title}
-    >
-      <RulesTableRest
-        policyId={profileId}
-        securityGuideId={securityGuideId}
-        total={rules?.meta.total}
-        rules={rules?.data}
-        remediationsEnabled={false}
-        columns={[Columns.Name, Columns.Severity, Columns.Remediation]}
-        ruleValues={{}}
-        loading={loading}
-        ruleTree={builtTree}
-      />
-    </PolicyRulesBase>
-  );
-};
-
-const PolicyRulesBase = ({
-  osMajorVersion,
-  benchmarkVersion,
-  headerName,
-  children,
-}) => {
-  return (
     <>
       <PolicyRulesHeader
-        name={headerName}
-        benchmarkVersion={benchmarkVersion}
-        osMajorVersion={osMajorVersion}
+        name={profileData?.data?.title}
+        benchmarkVersion={securityGuideData?.data?.version}
+        osMajorVersion={securityGuideData?.data?.os_major_version}
       />
       <div className="pf-v5-u-p-xl" style={{ background: '#fff' }}>
-        {children}
+        <RulesTable
+          policyId={profileId}
+          securityGuideId={securityGuideId}
+          total={rules?.meta.total}
+          rules={rules?.data}
+          remediationsEnabled={false}
+          columns={[Columns.Name, Columns.Severity, Columns.Remediation]}
+          ruleValues={{}}
+          loading={loading}
+          ruleTree={builtTree}
+        />
       </div>
     </>
   );
 };
 
-PolicyRulesBase.propTypes = {
-  osMajorVersion: propTypes.string,
-  benchmarkVersion: propTypes.string,
-  headerName: propTypes.string,
-  children: propTypes.any,
-};
-
 const PolicyRulesWrapper = () => (
   <TableStateProvider>
-    <GatedComponents
-      RestComponent={PolicyRulesRest}
-      GraphQLComponent={PolicyRulesGraphQL}
-    />
+    <PolicyDefaultRules />
   </TableStateProvider>
 );
 
