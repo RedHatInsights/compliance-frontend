e2dc3ba92d686010df69ff32ba16edb1ac6a489f
Egor Shamardin <33912805+LightOfHeaven1994@users.noreply.github.com>
2025-02-27 12:08:54 +0100

    chore(DeleteReport): Cleanup DeleteReport component (#2400)
    Co-authored-by: Aleksandr Voznesenskii <62722417+Fewwy@users.noreply.github.com>

--- src/SmartComponents/DeleteReport/DeleteReport.test.js
+++ src/SmartComponents/DeleteReport/DeleteReport.test.js
@@ -2,24 +2,13 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { useParams } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
 import useNavigate from '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate';
-import useAPIV2FeatureFlag from '../../Utilities/hooks/useAPIV2FeatureFlag';
 import { apiInstance } from '../../Utilities/hooks/useQuery';
-import DeleteReportWrapper from './DeleteReport';
+import DeleteReport from './DeleteReport';
 
 jest.mock('react-router-dom', () => ({
   useParams: jest.fn(),
 }));
-
-jest.mock('@apollo/client', () => {
-  const actualApolloClient = jest.requireActual('@apollo/client');
-  return {
-    ...actualApolloClient,
-    useMutation: jest.fn(),
-    gql: jest.fn().mockImplementation((query) => query),
-  };
-});
 
 jest.mock(
   '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate',
@@ -29,8 +18,6 @@ jest.mock(
   })
 );
 
-jest.mock('../../Utilities/hooks/useAPIV2FeatureFlag');
-jest.mock('../../Utilities/hooks/useQuery');
 jest.mock(
   '@redhat-cloud-services/frontend-components-notifications/redux',
   () => ({
@@ -38,11 +25,12 @@ jest.mock(
   })
 );
 
+jest.mock('../../Utilities/hooks/useQuery');
 jest.mock('Utilities/Dispatcher', () => ({
   dispatchAction: jest.fn(),
 }));
 
-describe('DeleteReportWrapper', () => {
+describe('DeleteReport', () => {
   const navigateMocked = jest.fn();
   useNavigate.mockImplementation(() => navigateMocked);
   const reportId = '12345';
@@ -52,19 +40,10 @@ describe('DeleteReportWrapper', () => {
     useParams.mockReturnValue({ report_id: reportId });
   });
 
-  test('renders spinner when apiV2Enabled is undefined', () => {
-    useAPIV2FeatureFlag.mockReturnValue(undefined);
-
-    render(<DeleteReportWrapper />);
-
-    expect(screen.getByRole('progressbar')).toBeInTheDocument();
-  });
-
   test('renders DeleteReportRest when apiV2Enabled is true', async () => {
-    useAPIV2FeatureFlag.mockReturnValue(true);
     apiInstance.deleteReport.mockResolvedValue({});
 
-    render(<DeleteReportWrapper />);
+    render(<DeleteReport />);
 
     fireEvent.click(screen.getByText('Delete report'));
 
@@ -75,31 +54,11 @@ describe('DeleteReportWrapper', () => {
     });
   });
 
-  test('renders DeleteReportGraphQL when apiV2Enabled is false', async () => {
-    const mockMutation = jest.fn(() => Promise.resolve());
-    useAPIV2FeatureFlag.mockReturnValue(false);
-    useMutation.mockReturnValue([
-      mockMutation,
-      { loading: false, error: null },
-    ]);
-
-    render(<DeleteReportWrapper />);
-
-    fireEvent.click(screen.getByText('Delete report'));
-
-    await waitFor(() => {
-      expect(mockMutation).toHaveBeenCalledWith({
-        variables: { input: { profileId: reportId } },
-      });
-    });
-  });
-
   test('handles error during REST deletion', async () => {
-    useAPIV2FeatureFlag.mockReturnValue(true);
     const error = new Error('Deletion failed');
     apiInstance.deleteReport.mockRejectedValue(error);
 
-    render(<DeleteReportWrapper />);
+    render(<DeleteReport />);
 
     fireEvent.click(screen.getByText('Delete report'));
 
