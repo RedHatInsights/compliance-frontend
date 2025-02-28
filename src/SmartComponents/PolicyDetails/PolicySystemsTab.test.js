14629ba4f1940de2579a824dbf69f60285633b91
LightOfHeaven1994 <eshamard@redhat.com>
2025-02-28 12:39:57 +0100

    chore(FeatureFlag): Remove v2 feature flag

--- src/SmartComponents/PolicyDetails/PolicySystemsTab.test.js
+++ src/SmartComponents/PolicyDetails/PolicySystemsTab.test.js
@@ -5,8 +5,6 @@ import TestWrapper from '@/Utilities/TestWrapper';
 import PolicySystemsTab from './PolicySystemsTab';
 import { policies } from '@/__fixtures__/policies';
 
-jest.mock('@/Utilities/hooks/useAPIV2FeatureFlag', () => jest.fn(() => false));
-
 describe('PolicySystemsTab', () => {
   it('expect to render with systems table', () => {
     const policy = {
