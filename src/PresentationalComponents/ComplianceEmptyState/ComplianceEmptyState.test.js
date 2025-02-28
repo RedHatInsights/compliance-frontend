49fd44aac9de830a4d9ce1080ddf27830d7b2c46
LightOfHeaven1994 <eshamard@redhat.com>
2025-02-28 10:30:36 +0100

    chore(ComplianceEmptyState): Cleanup CompEmptyState

--- src/PresentationalComponents/ComplianceEmptyState/ComplianceEmptyState.test.js
+++ src/PresentationalComponents/ComplianceEmptyState/ComplianceEmptyState.test.js
@@ -2,73 +2,47 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import TestWrapper from '@/Utilities/TestWrapper';
-import { useQuery } from '@apollo/client';
-import useAPIV2FeatureFlag from '@/Utilities/hooks/useAPIV2FeatureFlag.js';
 import usePolicies from 'Utilities/hooks/api/usePolicies';
+import { Button } from '@patternfly/react-core';
 
-jest.mock('@apollo/client');
-jest.mock('../../Utilities/hooks/useAPIV2FeatureFlag');
 jest.mock('Utilities/hooks/api/usePolicies', () => jest.fn());
 
 import ComplianceEmptyState from './ComplianceEmptyState';
 
 describe('ComplianceEmptyState', () => {
-  beforeEach(() => {
-    useAPIV2FeatureFlag.mockImplementation(() => false);
-  });
-  it('expect to render without error if no policies exist', () => {
-    useQuery.mockImplementation(() => ({
-      data: { profiles: { totalCount: 0 } },
+  it('expect to render with loading spinner when no results obtained yet', () => {
+    usePolicies.mockImplementation(() => ({
+      data: {},
       error: undefined,
+      loading: true,
+    }));
+    render(
+      <TestWrapper>
+        <ComplianceEmptyState />
+      </TestWrapper>
+    );
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('expect to render error state', () => {
+    const errorMsg = 'Something went wrong';
+    usePolicies.mockImplementation(() => ({
+      data: {},
+      error: errorMsg,
       loading: undefined,
     }));
     render(
       <TestWrapper>
-        <ComplianceEmptyState client={{}} />
+        <ComplianceEmptyState />
       </TestWrapper>
     );
 
-    expect(screen.getByText('No policies')).toBeInTheDocument();
-  });
-
-  it('expect to render different message if one policy exists', () => {
-    useQuery.mockImplementation(() => ({
-      data: { profiles: { totalCount: 1 } },
-      error: undefined,
-      loading: undefined,
-    }));
-    render(
-      <TestWrapper>
-        <ComplianceEmptyState client={{}} />
-      </TestWrapper>
-    );
-
-    expect(screen.getByText('1 policy')).toBeInTheDocument();
     expect(
-      screen.getByText('has been created but has no reports.')
+      screen.getByText(`Oops! Error loading System data: ${errorMsg}`)
     ).toBeInTheDocument();
   });
 
-  it('expect to render different message if many policies exist', () => {
-    useQuery.mockImplementation(() => ({
-      data: { profiles: { totalCount: 2 } },
-      error: undefined,
-      loading: undefined,
-    }));
-    render(
-      <TestWrapper>
-        <ComplianceEmptyState client={{}} />
-      </TestWrapper>
-    );
-
-    expect(screen.getByText('2 policies')).toBeInTheDocument();
-  });
-});
-
-describe('ComplianceEmptyState - REST', () => {
-  beforeEach(() => {
-    useAPIV2FeatureFlag.mockImplementation(() => true);
-  });
   it('expect to render without error if no policies exist', () => {
     usePolicies.mockImplementation(() => ({
       data: { meta: { total: 0 } },
@@ -77,7 +51,7 @@ describe('ComplianceEmptyState - REST', () => {
     }));
     render(
       <TestWrapper>
-        <ComplianceEmptyState client={{}} />
+        <ComplianceEmptyState />
       </TestWrapper>
     );
 
@@ -95,7 +69,7 @@ describe('ComplianceEmptyState - REST', () => {
     }));
     render(
       <TestWrapper>
-        <ComplianceEmptyState client={{}} />
+        <ComplianceEmptyState />
       </TestWrapper>
     );
 
@@ -114,7 +88,7 @@ describe('ComplianceEmptyState - REST', () => {
 
     render(
       <TestWrapper>
-        <ComplianceEmptyState client={{}} />
+        <ComplianceEmptyState />
       </TestWrapper>
     );
 
@@ -122,5 +96,33 @@ describe('ComplianceEmptyState - REST', () => {
     expect(
       screen.getByText('have been created but have no reports.')
     ).toBeInTheDocument();
+  });
+
+  it('expect to render with non-default props', () => {
+    usePolicies.mockImplementation(() => ({
+      data: { meta: { total: 1 } },
+      error: undefined,
+      loading: undefined,
+    }));
+
+    render(
+      <TestWrapper>
+        <ComplianceEmptyState
+          title="Cool title"
+          mainButton={
+            <Button
+              variant="primary"
+              component="a"
+              href="/insights/compliance/coolpage"
+            >
+              Nav to cool page
+            </Button>
+          }
+        />
+      </TestWrapper>
+    );
+
+    expect(screen.getByText('Cool title')).toBeInTheDocument();
+    expect(screen.getByText('Nav to cool page')).toBeInTheDocument();
   });
 });
