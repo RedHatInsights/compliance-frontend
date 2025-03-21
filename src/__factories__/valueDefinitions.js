7b1bda48de9a7a34ae34d2ca9638cead2f431704
LightOfHeaven1994 <eshamard@redhat.com>
2025-03-21 10:15:18 +0100

    Cypress tests for Tailorings component

---
+++ src/__factories__/valueDefinitions.js
@@ -0,0 +1,37 @@
+import { faker } from '@faker-js/faker';
+
+const buildValueDefinitions = (length, itemIds = []) => {
+  const count = itemIds.length > 0 ? itemIds.length : length;
+
+  return [...Array(count)].map((_, index) => {
+    const valueType = faker.helpers.arrayElement([
+      'string',
+      'integer',
+      'boolean',
+    ]);
+    let defaultValue;
+
+    switch (valueType) {
+      case 'integer':
+        defaultValue = faker.number.int({ min: 1, max: 100 }).toString();
+        break;
+      case 'boolean':
+        defaultValue = faker.helpers.arrayElement(['true', 'false']);
+        break;
+      case 'string':
+        defaultValue = faker.lorem.words({ min: 1, max: 2 });
+    }
+
+    return {
+      id: itemIds.length > 0 ? itemIds[index] : faker.string.uuid(),
+      ref_id: faker.lorem.slug({ min: 1, max: 4 }),
+      title: faker.lorem.words({ min: 2, max: 3 }),
+      description: faker.lorem.sentence({ min: 2, max: 4 }),
+      value_type: valueType,
+      default_value: defaultValue,
+      type: 'value_definition',
+    };
+  });
+};
+
+export default buildValueDefinitions;
