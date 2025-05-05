a6cc063a18eadc2c24a1ca9fe71a1c840223b704
Georgii Karataev <georgkarat@gmail.com>
2025-05-05 12:50:07 +0200

    chore: Align commitlint config
    No jira.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [1, 'always', 80],
-    'subject-case': [0],
+    'subject-case': [0, 'always'],
+    'footer-max-length': [0, 'always'],
+    'footer-max-line-length': [0, 'always'],
+    'body-max-length': [0, 'always'],
+    'body-max-line-length': [0, 'always'],
   },
 };
