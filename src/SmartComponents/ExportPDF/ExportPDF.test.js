e8489d25750e4c4ce33cbc97bb0b782468c493bf
LightOfHeaven1994 <eshamard@redhat.com>
2025-07-02 10:45:35 +0200

    feat(RHINENG-3019): Test ExportPDF component

--- src/SmartComponents/ExportPDF/ExportPDF.test.js
+++ src/SmartComponents/ExportPDF/ExportPDF.test.js
@@ -7,61 +7,22 @@ import { ReportDownload } from './ExportPDF';
 jest.mock('react-router-dom', () => ({
   useParams: jest.fn(() => ({ report_id: 'test-report-id' })),
 }));
+jest.mock('@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate', () => jest.fn(() => jest.fn()));
+jest.mock('SmartComponents/ReportDownload/hooks/useExportSettings', () => jest.fn(() => ({
+  exportSettings: {},
+  setExportSetting: jest.fn(),
+  isValid: true,
+})));
 
-// jest.mock('PresentationalComponents', () => ({
-//   ComplianceModal: jest.fn(({ children, actions, onClose, title, ...props }) => (
-//     <div data-testid="mock-compliance-modal" {...props}>
-//       <h2 data-testid="modal-title">{title}</h2>
-//       <button onClick={onClose} data-testid="modal-close-button">Close</button>
-//       {children}
-//       <div data-testid="mock-modal-actions">{actions}</div>
-//     </div>
-//   )),
-//   StateViewWithError: jest.fn(({ children, stateValues }) => {
-//     // This mock simplifies the StateViewWithError to directly render based on state
-//     if (stateValues.loading) {
-//       return <div data-testid="mock-state-view-loading">Loading content</div>;
-//     }
-//     if (stateValues.error) {
-//       return <div data-testid="mock-state-view-error">Error content</div>;
-//     }
-//     return <div data-testid="mock-state-view-data">{children}</div>;
-//   }),
-//   StateViewPart: jest.fn(({ children, stateKey }) => (
-//     <div data-testid={`mock-state-view-part-${stateKey}`}>{children}</div>
-//   )),
-// }));
+jest.mock('Utilities/hooks/api/useReport', () => jest.fn(() => ({
+  data: { data: { title: 'Test report title' } },
+  loading: false,
+  error: undefined,
+})));
 
-jest.mock(
-  '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate',
-  () => jest.fn(() => jest.fn()),
-);
-
-// jest.mock('SmartComponents/ReportDownload/Components/ExportPDFForm', () => jest.fn(() => (
-//   <div data-testid="mock-export-pdf-form">Mock Export PDF Form</div>
-// )));
-
-jest.mock('SmartComponents/ReportDownload/hooks/useExportSettings', () =>
-  jest.fn(() => ({
-    exportSettings: {},
-    setExportSetting: jest.fn(),
-    isValid: true,
-  })),
-);
-
-jest.mock('Utilities/hooks/api/useReport', () =>
-  jest.fn(() => ({
-    data: { data: { title: 'Test Report Title' } },
-    loading: false,
-    error: undefined,
-  })),
-);
-
-jest.mock('@redhat-cloud-services/frontend-components/useChrome', () =>
-  jest.fn(() => ({
-    requestPdf: jest.fn(),
-  })),
-);
+jest.mock('@redhat-cloud-services/frontend-components/useChrome', () => jest.fn(() => ({
+  requestPdf: jest.fn(),
+})));
 
 jest.mock('Utilities/Dispatcher', () => ({
   dispatchNotification: jest.fn(),
@@ -72,7 +33,7 @@ describe('ReportDownload', () => {
     render(<ReportDownload />);
 
     expect(screen.getByText('Compliance report'));
-    expect(screen.getByText('System data to include'));
+    expect(screen.getByText('Test report title'));
     expect(screen.getByText('Export to PDF'));
     expect(screen.getByText('Cancel'));
   });
