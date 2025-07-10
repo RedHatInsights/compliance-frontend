1e11026d922f39bddce61b3d46a90495cb36c3c1
Egor Shamardin <33912805+LightOfHeaven1994@users.noreply.github.com>
2025-07-10 16:41:57 +0200

    fix(RHINENG-17622): Update empty state message (#2527)

--- src/SmartComponents/SystemDetails/NoReportsState.js
+++ src/SmartComponents/SystemDetails/NoReportsState.js
@@ -4,28 +4,26 @@ import { CloudSecurityIcon } from '@patternfly/react-icons';
 import { Bullseye, EmptyState, EmptyStateBody } from '@patternfly/react-core';
 import { pluralize } from '@patternfly/react-core';
 
-const NoReportsState = ({ policiesCount }) => (
-  <Bullseye>
-    <EmptyState
-      headingLevel="h1"
-      icon={CloudSecurityIcon}
-      titleText="No results reported"
-      style={{
-        '--pf-v5-c-empty-state__icon--FontSize':
-          'var(--pf-v5-c-empty-state--m-xl__icon--FontSize)',
-      }}
-    >
-      <EmptyStateBody>
-        This system is part of {pluralize(policiesCount, 'policy', 'policies')},
-        but has not returned any results.
-      </EmptyStateBody>
-      <EmptyStateBody>
-        Reports are returned when the system checks into Insights. By default,
-        systems check in every 24 hours.
-      </EmptyStateBody>
-    </EmptyState>
-  </Bullseye>
-);
+const NoReportsState = ({ policiesCount }) => {
+  const bodyTextMain = `This system is part of ${pluralize(policiesCount, 'policy', 'policies')}, but has not returned any results.`;
+  const bodyTextSub = `Reports are returned after scanning the system using the 'insights-client --compliance' command.`;
+  return (
+    <Bullseye>
+      <EmptyState
+        headingLevel="h1"
+        icon={CloudSecurityIcon}
+        titleText="No results reported"
+        style={{
+          '--pf-v5-c-empty-state__icon--FontSize':
+            'var(--pf-v5-c-empty-state--m-xl__icon--FontSize)',
+        }}
+      >
+        <EmptyStateBody>{bodyTextMain}</EmptyStateBody>
+        <EmptyStateBody>{bodyTextSub}</EmptyStateBody>
+      </EmptyState>
+    </Bullseye>
+  );
+};
 
 NoReportsState.propTypes = {
   policiesCount: propTypes.number,
