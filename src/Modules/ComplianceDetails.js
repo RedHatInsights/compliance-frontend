4b3b3b95f008bd49a4a5895f416a459f5b9dadcf
Sebastian Gräßl <mail@bastilian.me>
2025-08-15 13:46:28 +0200

    fix(ComplianceDetails): Provide query client for fed module

--- src/Modules/ComplianceDetails.js
+++ src/Modules/ComplianceDetails.js
@@ -3,16 +3,21 @@ import { Provider } from 'react-redux';
 import { init } from 'Store';
 import Details from '../SmartComponents/SystemDetails/Details';
 import { RBACProvider } from '@redhat-cloud-services/frontend-components/RBACProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const ComplianceDetails = (props) => {
   const store = useRef(init().getStore());
 
   return (
-    <RBACProvider appName="compliance">
-      <Provider store={store.current}>
-        <Details {...props} />
-      </Provider>
-    </RBACProvider>
+    <QueryClientProvider client={queryClient}>
+      <RBACProvider appName="compliance">
+        <Provider store={store.current}>
+          <Details {...props} />
+        </Provider>
+      </RBACProvider>
+    </QueryClientProvider>
   );
 };
 
