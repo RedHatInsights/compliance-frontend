aabd48ec576d9fde7ef0a54781ca0dcca16b3087
LightOfHeaven1994 <eshamard@redhat.com>
2025-11-05 09:57:54 +0100

    fix(RHINENG-21782): Check for failed rules correctly

--- src/PresentationalComponents/ComplianceRemediationButton/hooks/useIssuesFetch/useIssuesFetch.js
+++ src/PresentationalComponents/ComplianceRemediationButton/hooks/useIssuesFetch/useIssuesFetch.js
@@ -33,8 +33,9 @@ export const useIssuesFetch = ({
 
   const canFetch = reportTestResults?.filter(
     (reportTestResult) =>
-      isReportTestResultSupported(reportTestResult) &&
-      reportTestResult?.rulesFailed > 0,
+      (isReportTestResultSupported(reportTestResult) &&
+        reportTestResult?.rulesFailed > 0) ||
+      reportTestResult?.failed_rule_count > 0,
   ).length;
 
   return {
