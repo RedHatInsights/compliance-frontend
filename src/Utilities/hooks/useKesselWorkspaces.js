fc1dacab2b3fbdf3d48b93d5258e86d2118c16f5
Egor Shamardin <33912805+LightOfHeaven1994@users.noreply.github.com>
2026-03-05 12:26:20 +0100

    chore(RHINENG-24318): Use shared workspace fetcher from kessel-sdk (#2747)

--- src/Utilities/hooks/useKesselWorkspaces.js
+++ src/Utilities/hooks/useKesselWorkspaces.js
@@ -1,47 +1,26 @@
-import { useMemo } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { APIFactory } from '@redhat-cloud-services/javascript-clients-shared';
-import listWorkspaces from '@redhat-cloud-services/rbac-client/v2/WorkspacesList';
-import { useAxiosWithPlatformInterceptors } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
-import { RBAC_API_BASE_V2 } from '@/constants';
+import { useState, useEffect } from 'react';
+import { fetchDefaultWorkspace } from '@project-kessel/react-kessel-access-check';
 
-const STALE_TIME = 5 * 60 * 1000; // could be Infinity ?
-
-export const useKesselWorkspaces = (options = {}) => {
-  const axios = useAxiosWithPlatformInterceptors();
-  const rbacClient = useMemo(
-    () =>
-      APIFactory(
-        RBAC_API_BASE_V2,
-        { workspacesList: listWorkspaces },
-        { axios },
-      ),
-    [axios],
-  );
-
-  return useQuery({
-    queryKey: ['workspaces', options.type, options.limit, options.name],
-    queryFn: async () => {
-      const response = await rbacClient.workspacesList({
-        limit: options.limit ?? 1000,
-        offset: 0,
-        type: options.type ?? 'all',
-        name: options.name,
-      });
-      return response?.data ?? [];
-    },
-    enabled: options.enabled ?? true,
-    staleTime: options.staleTime,
-  });
-};
+let defaultWorkspacePromise = null;
 
 export const useFetchDefaultWorkspaceId = () => {
-  const {
-    data: workspaces,
-    isLoading,
-    error,
-  } = useKesselWorkspaces({ type: 'default', limit: 1, staleTime: STALE_TIME });
-  const defaultWorkspace = workspaces?.[0];
+  const [defaultWorkspace, setDefaultWorkspace] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const baseUrl = window.location.origin;
+
+  useEffect(() => {
+    if (!defaultWorkspacePromise) {
+      defaultWorkspacePromise = fetchDefaultWorkspace(baseUrl);
+    }
+    defaultWorkspacePromise
+      .then(setDefaultWorkspace)
+      .catch((err) => {
+        defaultWorkspacePromise = null;
+        setError(err);
+      })
+      .finally(() => setIsLoading(false));
+  }, [baseUrl]);
 
   return {
     workspaceId: defaultWorkspace?.id,
