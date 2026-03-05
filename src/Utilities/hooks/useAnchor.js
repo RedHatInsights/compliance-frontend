e5b45d013b69c1b8c97e1ab5497ced98b12b9f52
Egor Shamardin <33912805+LightOfHeaven1994@users.noreply.github.com>
2026-03-05 11:37:11 +0100

    fix(RHINENG-22155): Do not allow override tab anchors (#2752)

--- src/Utilities/hooks/useAnchor.js
+++ src/Utilities/hooks/useAnchor.js
@@ -1,10 +1,17 @@
 import { useLocation } from 'react-router-dom';
 
+const isCorrectTabHash = (hash) =>
+  hash && hash.length > 1 && !hash.replace('#', '').includes('=');
+
+const lastTabByPath = {};
+
 export const useAnchor = (defaultAnchor = '') => {
-  const location = useLocation();
-  const hash =
-    location.hash && location.hash.length > 0 ? location.hash : undefined;
-  return (hash || defaultAnchor).replace('#', '');
+  const { pathname, hash } = useLocation();
+  const raw = hash && hash.length > 0 ? hash.replace('#', '') : '';
+  if (isCorrectTabHash(hash)) lastTabByPath[pathname] = raw;
+  return isCorrectTabHash(hash)
+    ? raw
+    : (lastTabByPath[pathname] ?? defaultAnchor.replace('#', ''));
 };
 
 export default useAnchor;
