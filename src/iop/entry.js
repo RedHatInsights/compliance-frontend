560e2ac18ee0c151c2925c542d253f5b11699a44
LightOfHeaven1994 <eshamard@redhat.com>
2026-06-23 15:33:26 +0200

    feat(IoP): cleanup

--- src/iop/entry.js
+++ src/iop/entry.js
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 
-// HCC injects Patternfly via insights-chrome head snippets; IoP iframe has no chrome host.
 import '@patternfly/patternfly/patternfly.scss';
 import '@patternfly/patternfly/patternfly-addons.scss';
 import '@patternfly/patternfly/utilities/Accessibility/accessibility.scss';
