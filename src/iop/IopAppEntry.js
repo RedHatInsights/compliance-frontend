522e9f82a133056ced38314f7da56961a2dbbd6d
LightOfHeaven1994 <eshamard@redhat.com>
2026-06-23 14:19:36 +0200

    feat(IoP): add styling

--- src/iop/IopAppEntry.js
+++ src/iop/IopAppEntry.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { FlagProvider } from '@unleash/proxy-client-react';
+import { Page } from '@patternfly/react-core';
 
 import { init } from 'Store';
 import App from '../App';
@@ -10,15 +11,17 @@ import { staticUnleashFlagProviderConfig } from '@/config/staticUnleashFlagProvi
 import IopBridge from './IopBridge';
 
 const IopAppEntry = ({ environment = 'production' }) => (
-  <HashRouter>
-    <IopBridge>
-      <FlagProvider config={staticUnleashFlagProviderConfig}>
-        <Provider store={init(environment).getStore()}>
-          <App />
-        </Provider>
-      </FlagProvider>
-    </IopBridge>
-  </HashRouter>
+  <Page className="iop-compliance-page" sidebar={null}>
+    <HashRouter>
+      <IopBridge>
+        <FlagProvider config={staticUnleashFlagProviderConfig}>
+          <Provider store={init(environment).getStore()}>
+            <App />
+          </Provider>
+        </FlagProvider>
+      </IopBridge>
+    </HashRouter>
+  </Page>
 );
 
 IopAppEntry.propTypes = {
--- src/iop/entry.js
+++ src/iop/entry.js
@@ -1,7 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 
+// HCC injects Patternfly via insights-chrome head snippets; IoP iframe has no chrome host.
+import '@patternfly/patternfly/patternfly.scss';
+import '@patternfly/patternfly/patternfly-addons.scss';
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.scss';
+import '@patternfly/patternfly/utilities/Spacing/spacing.scss';
+import '@patternfly/patternfly/patternfly-charts.scss';
+
 import IopAppEntry from './IopAppEntry';
+import './iopPage.scss';
 
 const mountNode = document.getElementById('root');
 
