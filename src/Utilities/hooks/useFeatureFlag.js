69a303743fb27f62569e8f07fa9426416a8a664b
LightOfHeaven1994 <eshamard@redhat.com>
2026-07-08 11:21:58 +0200

    feat(Compliance): remove iopFlagProvider

--- src/Utilities/hooks/useFeatureFlag.js
+++ src/Utilities/hooks/useFeatureFlag.js
@@ -4,12 +4,12 @@ import { getAppConfig } from '@/config/appConfig';
 import { getIopStaticFeatureFlag } from '@/config/iopStaticFeatureFlags';
 
 export default (flag) => {
-  const { flagsReady } = useFlagsStatus();
-  const isFlagEnabled = useFlag(flag);
-
   if (!getAppConfig().features.unleash) {
     return getIopStaticFeatureFlag(flag);
   }
+
+  const { flagsReady } = useFlagsStatus();
+  const isFlagEnabled = useFlag(flag);
 
   return flagsReady ? isFlagEnabled : undefined;
 };
